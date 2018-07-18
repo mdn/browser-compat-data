5c361e3f37278d096b2b552bc6bffb6cfe39d2f2
Claas Augner <mozilla@caugner.de>
2018-07-18 22:41:14 +0200

    test: add fix script

---
+++ test/fix-consistency.js
@@ -0,0 +1,137 @@
+'use strict';
+
+const fs = require('fs');
+const path = require('path');
+
+/**
+ * Consistency check.
+ * 
+ * This fixer aims at improving data quality
+ * by repairing inconsistent information.
+ */
+ class ConsistencyFixer
+ {
+  /**
+   * @param {object} data
+   * @returns {object}
+   */
+  fix(data) {
+    const clone = JSON.parse(JSON.stringify(data));
+    return this.fixSubfeatures(clone);
+  }
+
+  /**
+   * @param {object} data
+   * @returns {object}
+   */
+  fixSubfeatures(data) {
+    if (typeof data !== 'object') {
+      return data;
+    }
+
+    // Fix this feature.
+    if (this.isFeature(data)) {
+      data = this.fixFeature(data);
+    }
+  
+    // Fix sub-features.
+    const keys = Object.keys(data);
+    for (let key of keys) {
+      if (key != '__compat') {
+        data[key] = this.fixSubfeatures(data[key]);
+      }
+    }
+  
+    return data;
+  }
+
+  /**
+   * @param {object} data
+   * @returns {object}
+   */
+  fixFeature(data) {
+    const unsupportedInParent = this.extractUnsupportedBrowsers(data.__compat);
+
+    // For all unsupported browsers, sub-features should be set to false.
+    const subfeatures = Object.keys(data).filter(key => this.isFeature(data[key]));
+    const inconsistentSubfeaturesByBrowser = {};
+
+    for (let subfeature of subfeatures) {
+      const unsupportedInChild = this.extractUnsupportedBrowsers(data[subfeature].__compat);
+
+      const browsers = unsupportedInParent.filter(x => !unsupportedInChild.includes(x));
+
+      const support = data[subfeature].__compat.support;
+      for (let browser of browsers) {
+        if (typeof support[browser] === 'object') {
+          support[browser].version_added = false;
+        } else {
+          support[browser] = {
+            version_added: false
+          };
+        }
+      };
+    }
+
+    return data;
+  }
+
+  /**
+   * @param {object} data
+   * @returns {boolean}
+   */
+  isFeature(data) {
+    return typeof(data) === 'object' && '__compat' in data;
+  }
+
+  /**
+   * @param {object} compatData
+   * @returns {Array<string>}
+   */
+  extractUnsupportedBrowsers(compatData) {
+    return this.extractBrowsers(compatData, data => data.version_added === false);;
+  }
+  
+  /**
+   * 
+   * @param {object} compatData
+   * @param {callback} callback
+   * @returns {boolean}
+   */
+  extractBrowsers(compatData, callback)
+  {
+    return Object.keys(compatData.support).filter(browser => {
+      const browserData = compatData.support[browser];
+  
+      if (Array.isArray(browserData)) {
+        return browserData.every(callback);
+      } else if (typeof browserData === 'object') {
+        return callback(browserData);
+      } else {
+        return false;
+      }
+    });
+  }
+ }
+
+ /**
+  * @param {Promise<void>} filename 
+  */
+function fixConsistency(filename) {
+  const originalData = require(filename);
+
+  const fixer = new ConsistencyFixer();
+  const fixedData = fixer.fix(originalData);
+
+
+  if (JSON.stringify(originalData) === JSON.stringify(fixedData)) {
+    return;
+  }
+
+  console.log("Fixed consistency: " + path.relative(path.resolve(__dirname, '..'), filename));
+
+  const json = JSON.stringify(fixedData, null, 2);
+  fs.writeFileSync(filename, json, 'utf8');
+}
+
+module.exports.fixConsistency = fixConsistency;
---
+++ test/fix.js
@@ -0,0 +1,53 @@
+'use strict';
+const fs = require('fs');
+const path = require('path');
+const {fixConsistency} = require('./fix-consistency');
+
+
+const promises = [];
+
+/**
+ * @param {string[]} files
+ */
+function load(...files) {
+  for (let file of files) {
+    if (file.indexOf(__dirname) !== 0) {
+      file = path.resolve(__dirname, '..', file);
+    }
+
+    if (!fs.existsSync(file)) {
+      continue; // Ignore non-existent files
+    }
+
+    if (fs.statSync(file).isFile()) {
+      if (path.extname(file) === '.json') {
+        fixConsistency(file);
+      }
+
+      continue;
+    }
+
+    const subFiles = fs.readdirSync(file).map((subfile) => {
+      return path.join(file, subfile);
+    });
+
+    load(...subFiles);
+  }
+}
+
+if (process.argv[2]) {
+  load(process.argv[2]);
+} else {
+  load(
+    'api',
+    'css',
+    'html',
+    'http',
+    'svg',
+    'javascript',
+    'mathml',
+    'test',
+    'webdriver',
+    'webextensions'
+  );
+}
