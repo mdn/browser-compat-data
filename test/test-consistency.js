f38f502bd4c3d9d9cc1e8d5a4ee765a0e6212886
Vinyl Darkscratch <vinyldarkscratch@gmail.com>
2018-10-03 23:06:06 -0700

    Add basic version_added mismatch test

--- test/test-consistency.js
+++ test/test-consistency.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const path = require('path');
+const compareVersions = require('compare-versions');
 
 /**
  * Consistency check.
@@ -59,11 +60,14 @@ const path = require('path');
    * @returns {Array<object>}
    */
   checkFeature(data) {
-    const unsupportedInParent = this.extractUnsupportedBrowsers(data.__compat);
+    let errors = [];
 
-    // For all unsupported browsers, sub-features should be set to false.
     const subfeatures = Object.keys(data).filter(key => this.isFeature(data[key]));
-    const inconsistentSubfeaturesByBrowser = {};
+
+    // Test whether sub-features are supported when basic support is not implemented
+    // For all unsupported browsers (basic support == false), sub-features should be set to false
+    const unsupportedInParent = this.extractUnsupportedBrowsers(data.__compat);
+    var inconsistentSubfeaturesByBrowser = {};
 
     subfeatures.forEach(subfeature => {
       const unsupportedInChild = this.extractUnsupportedBrowsers(data[subfeature].__compat);
@@ -76,12 +80,38 @@ const path = require('path');
       });
     });
 
-    // Collect errors.
-    let errors = [];
+    // Add errors
     Object.keys(inconsistentSubfeaturesByBrowser).forEach(browser => {
       const subfeatures = inconsistentSubfeaturesByBrowser[browser];
+      const errortype = 'unsupported';
 
       errors.push({
+        errortype,
+        browser,
+        subfeatures
+      });
+    });
+
+    // Test whether sub-features are supported at an earlier version than basic support
+    const supportInParent = this.extractSupportedBrowsersWithVersion(data.__compat);
+    inconsistentSubfeaturesByBrowser = {};
+
+    subfeatures.forEach(subfeature => {
+      supportInParent.forEach(browser => {
+        if (data[subfeature].__compat.support[browser] != undefined && this.isVersionGreater(data[subfeature].__compat.support[browser], data.__compat.support[browser])) {
+          inconsistentSubfeaturesByBrowser[browser] = inconsistentSubfeaturesByBrowser[browser] || [];
+          inconsistentSubfeaturesByBrowser[browser].push(subfeature);
+        }
+      });
+    });
+
+    // Add errors
+    Object.keys(inconsistentSubfeaturesByBrowser).forEach(browser => {
+      const subfeatures = inconsistentSubfeaturesByBrowser[browser];
+      const errortype = 'version_mismatch';
+
+      errors.push({
+        errortype,
         browser,
         subfeatures
       });
@@ -104,6 +134,27 @@ const path = require('path');
    */
   extractUnsupportedBrowsers(compatData) {
     return this.extractBrowsers(compatData, data => data.version_added === false || typeof data.version_removed !== 'undefined' && data.version_removed !== false);
+  }
+
+  /**
+   * @param {object} compatData
+   * @returns {Array<string>}
+   */
+  extractSupportedBrowsersWithVersion(compatData) {
+    return this.extractBrowsers(compatData, data => typeof(data.version_added) === 'string');
+  }
+
+  /*
+   * @param {string} a
+   * @param {string} b
+   * @returns {boolean}
+   */
+  isVersionGreater(a, b) {
+    // XXX Only works with regular dictionaries that contain a version_added string -- needs to check against arrays
+    // XXX Should check against version_removed
+
+    if (typeof(a.version_added) === 'string' && typeof(b.version_added) === 'string') return compareVersions(a.version_added, b.version_added) == -1;
+    return false;
   }
   
   /**
@@ -139,7 +190,13 @@ function testConsistency(filename) {
     console.error(`\x1b[34m  Found \x1b[1m${errors.length}\x1b[0m\x1b[34m inconsistent feature(s) in \x1b[3m${relativeFilename}:\x1b[0m`);
     errors.forEach(({ feature, path, errors }) =>  {
       console.error(`\x1b[34m  → \x1b[1m${errors.length}\x1b[0m\x1b[34m × \x1b[1m${feature}\x1b[0m\x1b[34m [\x1b[3m${path.join('.')}\x1b[0m\x1b[34m]: `);
-      errors.forEach(({ browser, subfeatures }) => console.error(`\x1b[34m    → No support in \x1b[1m${browser}\x1b[0m\x1b[34m, but this is not declared for sub-feature(s): \x1b[1m${subfeatures.join(', ')}\x1b[0m`));
+      errors.forEach(({ errortype, browser, subfeatures }) => {
+        if (errortype == "unsupported") {
+          console.error(`\x1b[34m    → No support in \x1b[1m${browser}\x1b[0m\x1b[34m, but this is not declared for sub-feature(s): \x1b[1m${subfeatures.join(', ')}\x1b[0m`);
+        } else if (errortype == "version_mismatch") {
+          console.error(`\x1b[34m    → Basic support in \x1b[1m${browser}\x1b[0m\x1b[34m was declared implemented in a later version than the following sub-feature(s): \x1b[1m${subfeatures.join(', ')}\x1b[0m`);
+        }
+      });
     })
     return true;
   } else {
