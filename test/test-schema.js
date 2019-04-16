509674da45254d87bb15fb2db65b72da60c2b362
ExE Boss <3889017+ExE-Boss@users.noreply.github.com>
2019-04-16 19:01:03 +0200

    feat(TypeScript): Update types (#3739)
    * feat(TypeScript): Update types
    
    * fix(types): Remove wrong `export as namespace`

--- test/test-schema.js
+++ test/test-schema.js
@@ -5,6 +5,10 @@ const path = require('path');
 
 const ajv = new Ajv({ jsonPointers: true, allErrors: true });
 
+/**
+ * @param {string} dataFilename
+ * @param {string} [schemaFilename]
+ */
 function testSchema(dataFilename, schemaFilename = './../schemas/compat-data.schema.json') {
   const schema = require(schemaFilename);
   const data   = require(dataFilename);
