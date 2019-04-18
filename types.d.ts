ac1fe4c36c76451914f50a0b42d6779db0e8b1c6
ExE Boss <3889017+ExE-Boss@users.noreply.github.com>
2019-04-18 03:40:39 +0200

    Add engines to browsers in TypeScript (#1)

--- types.d.ts
+++ types.d.ts
@@ -1,8 +1,6 @@
 /* Any copyright is dedicated to the Public Domain.
  * http://creativecommons.org/publicdomain/zero/1.0/ */
 
-export as namespace bcd;
-
 /**
  * The names of the known browsers.
  */
@@ -24,6 +22,15 @@ export type BrowserNames =
   | 'uc_android'
   | 'uc_chinese_android'
   | 'webview_android';
+
+export type BrowserEngineNames =
+  | 'Blink'
+  | 'EdgeHTML'
+  | 'Gecko'
+  | 'Presto'
+  | 'Trident'
+  | 'WebKit'
+  | 'V8';
 
 /**
  * The browser namespace.
@@ -71,6 +78,16 @@ export interface ReleaseStatement {
    * The URL of the release notes.
    */
   release_notes?: string;
+
+  /**
+   * Name of the browser's underlying engine.
+   */
+  engine?: BrowserEngineNames;
+
+  /**
+   * Version of the engine corresponding to the browser version.
+   */
+  engine_version?: string;
 
   /**
    * A property indicating where in the lifetime cycle this release is in.
@@ -240,6 +257,8 @@ export interface CompatStatement {
    */
   mdn_url?: string;
 
+  matches?: MatchesBlock;
+
   /**
    * Each `__compat` object contains support information.
    *
@@ -260,6 +279,12 @@ export interface CompatStatement {
 export interface SupportBlock
   extends Partial<Record<BrowserNames, SupportStatement>>,
     Partial<Record<string, SupportStatement>> {}
+
+export interface MatchesBlock {
+  keywords?: string[];
+  regex_token?: string;
+  regex_value?: string;
+}
 
 /**
  * The status property contains information about stability of the feature.
