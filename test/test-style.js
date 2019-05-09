7560146b0c94b2e16a033195e3b2c632118dfc50
Vinyl Darkscratch <vinyldarkscratch@gmail.com>
2019-05-09 02:57:59 -0700

    Re-remove redundant "file" console logging

--- test/test-style.js
+++ test/test-style.js
@@ -145,13 +145,11 @@ function testStyle(filename) {
 
   if (actual !== expected) {
     hasErrors = true;
-    console.error(chalk.red(`  File : ${path.relative(process.cwd(), filename)}`));
     console.error(chalk`{red   Style – Error on }{red.bold line ${jsonDiff(actual, expected)}}`);
   }
 
   if (expected !== expectedSorting) {
     hasErrors = true;
-    console.error(chalk.red(`  File : ${path.relative(process.cwd(), filename)}`));
     console.error(chalk`{red   Browser name sorting – Error on }{red.bold line ${jsonDiff(expected, expectedSorting)}}`);
   }
 
