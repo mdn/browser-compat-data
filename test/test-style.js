e9bb3853610b0f4413b25009aadeac6d686e708a
Queen Vinyl Darkscratch <vinyldarkscratch@gmail.com>
2019-07-10 12:00:36 -0700

    Use new chalk templating

--- test/test-style.js
+++ test/test-style.js
@@ -188,13 +188,12 @@ function processData(filename, logger) {
 
   if (expected !== expectedBrowserSorting) {
     hasErrors = true;
-    logger.error(chalk`{red Browser sorting error on {bold line ${jsonDiff(expected, expectedSorting)}}}`);
-    logger.error(chalk`{red Browser sorting error on }{red.bold line ${jsonDiff(expected, expectedBrowserSorting)}}\n{blue     Tip: Run }{cyan.bold npm run fix }{blue to fix sorting automatically}`);
+    logger.error(chalk`{red Browser sorting error on {bold line ${jsonDiff(expected, expectedSorting)}}}\n{blue     Tip: Run {bold npm run fix} to fix sorting automatically}`);
   }
 
   if (actual !== expectedFeatureSorting) {
     hasErrors = true;
-    logger.error(chalk`{red Feature sorting error on }{red.bold line ${jsonDiff(expected, expectedFeatureSorting)}}\n{blue     Tip: Run }{cyan.bold npm run fix }{blue to fix sorting automatically}`);
+    logger.error(chalk`{red Feature sorting error on {bold line ${jsonDiff(expected, expectedFeatureSorting)}}}\n{blue     Tip: Run {bold npm run fix} to fix sorting automatically}`);
   }
 
   const bugzillaMatch = actual.match(String.raw`https?://bugzilla\.mozilla\.org/show_bug\.cgi\?id=(\d+)`);
