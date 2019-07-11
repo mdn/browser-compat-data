0b50386254f9e558008dc144550f34b8b590b7a8
ExE Boss <3889017+ExE-Boss@users.noreply.github.com>
2019-07-11 15:40:00 +0200

    test(versions): Restrict `<=` to WebView 37

--- test/test-versions.js
+++ test/test-versions.js
@@ -17,14 +17,22 @@ for (const browser of Object.keys(browsers)) {
   validBrowserVersions[browser] = Object.keys(browsers[browser].releases);
 }
 
+/** @type {Object<string, string[]>} */
+const VERSION_RANGE_BROWSERS = {
+  webview_android: ['37'],
+};
+
 /**
  * @param {string} browserIdentifier
  * @param {VersionValue} version
  */
 function isValidVersion(browserIdentifier, version) {
   if (typeof version === 'string') {
-    if (version.startsWith('<=')) {
-      version = version.substring(2);
+    if (VERSION_RANGE_BROWSERS[browserIdentifier] && version.startsWith('<=')) {
+      let realVersion = version.substring(2);
+      if (VERSION_RANGE_BROWSERS[browserIdentifier].includes(realVersion)) {
+        version = realVersion;
+      }
     }
     return validBrowserVersions[browserIdentifier].includes(version);
   } else {
