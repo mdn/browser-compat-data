4c5e291bcbecf253b94296e788508644a0ffa92a
ExE Boss <3889017+ExE-Boss@users.noreply.github.com>
2019-08-26 03:00:00 +0200

    test(links): Refactor code to use a helper function

--- test/test-links.js
+++ test/test-links.js
@@ -25,76 +25,75 @@ function processData(filename, logger) {
     expected = expected.replace(/\r/g, '');
   }
 
-  const bugzillaMatch = actual.match(String.raw`https?://bugzilla\.mozilla\.org/show_bug\.cgi\?id=(\d+)`);
-  if (bugzillaMatch) {
-    // use https://bugzil.la/1000000 instead
-    hasErrors = true;
-    logger.error(chalk`{red ${indexToPos(actual, bugzillaMatch.index)} – Use shortenable URL ({yellow ${bugzillaMatch[0]} → {green {bold https://bugzil.la/}${bugzillaMatch[1]}}).}`);
-  }
+  /** @type {(regexp: string|RegExp, matchHandler: (match: RegExpMatchArray) => void) => void} */
+  const processLinks = PROCESS_LINKS.bind(this, actual);
 
-  {
+  processLinks(
+    String.raw`https?://bugzilla\.mozilla\.org/show_bug\.cgi\?id=(\d+)`,
+    match => {
+      // use https://bugzil.la/1000000 instead
+      hasErrors = true;
+      logger.error(chalk`{red ${indexToPos(actual, match.index)} – Use shortenable URL ({yellow ${match[0]} → {green {bold https://bugzil.la/}${match[1]}}).}`);
+    }
+  );
+
+  processLinks(
+    String.raw`https?://bugs\.chromium\.org/p/chromium/issues/detail\?id=(\d+)`,
+    match => {
+      // use https://crbug.com/100000 instead
+      hasErrors = true;
+      logger.error(chalk`{red ${indexToPos(actual, match.index)} – Use shortenable URL ({yellow ${match[0]}} → {green {bold https://crbug.com/}${match[1]}}).}`);
+    }
+  );
+
+  processLinks(
+    String.raw`https?://bugs\.webkit\.org/show_bug\.cgi\?id=(\d+)`,
+    match => {
+      // use https://webkit.org/b/100000 instead
+      hasErrors = true;
+      logger.error(chalk`{red ${indexToPos(actual, match.index)} – Use shortenable URL ({yellow ${match[0]}} → {green {bold https://webkit.org/b/}${match[1]}}).}`);
+    }
+  );
+
+  processLinks(
     // Bugzil.la links should use HTTPS and have "bug ###" as link text ("Bug ###" only at the begin of notes/sentences).
-    const regexp = new RegExp(String.raw`(....)<a href='(https?)://(bugzil\.la|crbug\.com|webkit\.org/b)/(\d+)'>(.*?)</a>`, 'g');
-    /** @type {RegExpExecArray} */
-    let match;
-    do {
-      match = regexp.exec(actual);
-      if (match) {
-        const [,
-          before,
-          protocol,
-          domain,
-          bugId,
-          linkText,
-        ] = match;
+    String.raw`(....)<a href='(https?)://(bugzil\.la|crbug\.com|webkit\.org/b)/(\d+)'>(.*?)</a>`,
+    match => {
+      const [,
+        before,
+        protocol,
+        domain,
+        bugId,
+        linkText,
+      ] = match;
 
-        if (protocol !== 'https') {
-          hasErrors = true;
-          logger.error(chalk`{red ${indexToPos(actual, match.index)} – Use HTTPS URL ({yellow http://${domain}/${bugId}} → {green http{bold s}://${domain}/${bugId}}).}`);
-        }
-
-        if (domain !== 'bugzil.la') {
-          continue;
-        }
-
-        if (/^bug $/.test(before)) {
-          hasErrors = true;
-          logger.error(chalk`{red ${indexToPos(actual, match.index)} – Move word "bug" into link text ({yellow "${before}<a href='...'>${linkText}</a>"} → {green "<a href='...'>{bold ${before}}${bugId}</a>"}).}`);
-        } else if (linkText === `Bug ${bugId}`) {
-          if (!/(\. |")$/.test(before)) {
-            hasErrors = true;
-            logger.error(chalk`{red ${indexToPos(actual, match.index)} – Use lowercase "bug" word within sentence ({yellow "Bug ${bugId}"} → {green "{bold bug} ${bugId}"}).}`);
-          }
-        } else if (linkText !== `bug ${bugId}`) {
-          hasErrors = true;
-          logger.error(chalk`{red ${indexToPos(actual, match.index)} – Use standard link text ({yellow "${linkText}"} → {green "bug ${bugId}"}).}`);
-        }
+      if (protocol !== 'https') {
+        hasErrors = true;
+        logger.error(chalk`{red ${indexToPos(actual, match.index)} – Use HTTPS URL ({yellow http://${domain}/${bugId}} → {green http{bold s}://${domain}/${bugId}}).}`);
       }
-    } while (match != null);
-  }
 
-  const crbugMatch = actual.match(String.raw`https?://bugs\.chromium\.org/p/chromium/issues/detail\?id=(\d+)`);
-  if (crbugMatch) {
-    // use https://crbug.com/100000 instead
-    hasErrors = true;
-    logger.error(chalk`{red ${indexToPos(actual, crbugMatch.index)} – Use shortenable URL ({yellow ${crbugMatch[0]}} → {green {bold https://crbug.com/}${crbugMatch[1]}}).}`);
-  }
+      if (domain !== 'bugzil.la') {
+        return;
+      }
 
-  const webkitMatch = actual.match(String.raw`https?://bugs\.webkit\.org/show_bug\.cgi\?id=(\d+)`);
-  if (webkitMatch) {
-    // use https://webkit.org/b/100000 instead
-    hasErrors = true;
-    logger.error(chalk`{red ${indexToPos(actual, webkitMatch.index)} – Use shortenable URL ({yellow ${webkitMatch[0]}} → {green {bold https://webkit.org/b/}${webkitMatch[1]}}).}`);
-  }
+      if (/^bug $/.test(before)) {
+        hasErrors = true;
+        logger.error(chalk`{red ${indexToPos(actual, match.index)} – Move word "bug" into link text ({yellow "${before}<a href='...'>${linkText}</a>"} → {green "<a href='...'>{bold ${before}}${bugId}</a>"}).}`);
+      } else if (linkText === `Bug ${bugId}`) {
+        if (!/(\. |")$/.test(before)) {
+          hasErrors = true;
+          logger.error(chalk`{red ${indexToPos(actual, match.index)} – Use lowercase "bug" word within sentence ({yellow "Bug ${bugId}"} → {green "{bold bug} ${bugId}"}).}`);
+        }
+      } else if (linkText !== `bug ${bugId}`) {
+        hasErrors = true;
+        logger.error(chalk`{red ${indexToPos(actual, match.index)} – Use standard link text ({yellow "${linkText}"} → {green "bug ${bugId}"}).}`);
+      }
+    }
+  )
 
-  {
-    const regexp = new RegExp(
-      String.raw`\b(https?)://((?:[a-z][a-z0-9-]*\.)*)developer.mozilla.org/(.*?)(?=["'\s])`,
-      'g',
-    );
-    /** @type {RegExpExecArray} */
-    let match;
-    while ((match = regexp.exec(actual)) !== null) {
+  processLinks(
+    String.raw`\b(https?)://((?:[a-z][a-z0-9-]*\.)*)developer.mozilla.org/(.*?)(?=["'\s])`,
+    match => {
       const [url, protocol, subdomain, path] = match;
       let [, locale, expectedPath] = /^(?:(\w\w(?:-\w\w)?)\/)?(.*)$/.exec(path);
 
@@ -145,15 +144,31 @@ function processData(filename, logger) {
         );
       }
     }
-  }
+  );
 
-  const msdevUrlMatch = actual.match(String.raw`https?://developer.microsoft.com/(\w\w-\w\w)/(.*?)(?=["'\s])`);
-  if (msdevUrlMatch) {
-    hasErrors = true;
-    logger.error(chalk`{red ${indexToPos(actual, msdevUrlMatch.index)} – Use non-localized Microsoft Developer URL ({yellow ${msdevUrlMatch[0]}} → {green https://developer.microsoft.com/${msdevUrlMatch[2]}}).}`);
-  }
+  processLinks(
+    String.raw`https?://developer.microsoft.com/(\w\w-\w\w)/(.*?)(?=["'\s])`,
+    match => {
+      hasErrors = true;
+      logger.error(chalk`{red ${indexToPos(actual, match.index)} – Use non-localized Microsoft Developer URL ({yellow ${match[0]}} → {green https://developer.microsoft.com/${match[2]}}).}`);
+    }
+  );
 
   return hasErrors;
+}
+
+/**
+ * @param {string} actual
+ * @param {string|RegExp} regexp
+ * @param {(match: RegExpExecArray) => void} matchHandler
+ */
+const PROCESS_LINKS = function processLinks(actual, regexp, matchHandler) {
+  const re = new RegExp(regexp, 'g');
+  /** @type {RegExpExecArray} */
+  let match;
+  while ((match = re.exec(actual)) !== null) {
+    matchHandler(match);
+  }
 }
 
 /**
