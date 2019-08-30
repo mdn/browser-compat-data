4e2cae71ea38b1a0dace25e9fe2066b8a5898378
Queen Vinyl Darkscratch <vinyldarkscratch@gmail.com>
2019-08-29 18:31:18 -0700

    Get version mapping from BCD itself

--- scripts/mirror.js
+++ scripts/mirror.js
@@ -7,6 +7,8 @@ const fs = require('fs');
 const path = require('path');
 const { platform } = require('os');
 
+const browsers = require('..').browsers;
+
 const { argv } = require('yargs').command('$0 <browser> [file]', 'Mirror values onto a specified browser if "version_added" is true/null, based upon its parent or a specified source', (yargs) => {
   yargs.positional('browser', {
     describe: 'The destination browser',
@@ -26,21 +28,20 @@ const { argv } = require('yargs').command('$0 <browser> [file]', 'Mirror values 
   });
 });
 
-const samsunginternet_mapping = {
-  "1": "1.0", "2": "1.0", "3": "1.0", "4": "1.0", "5": "1.0", "6": "1.0", "7": "1.0", "8": "1.0", "9": "1.0", "10": "1.0", "11": "1.0", "12": "1.0", "13": "1.0", "14": "1.0", "15": "1.0", "16": "1.0", "17": "1.0", "18": "1.0",
-  "19": "1.5", "20": "1.5", "21": "1.5", "22": "1.5", "23": "1.5", "24": "1.5", "25": "1.5", "26": "1.5", "27": "1.5", "28": "1.5",
-  "29": "2.0", "30": "2.0", "31": "2.0", "32": "2.0", "33": "2.0", "34": "2.0",
-  "35": "3.0", "36": "3.0", "37": "3.0", "38": "3.0",
-  "39": "4.0", "40": "4.0", "41": "4.0", "42": "4.0", "43": "4.0", "44": "4.0",
-  "45": "5.0", "46": "5.0", "47": "5.0", "48": "5.0", "49": "5.0", "50": "5.0", "51": "5.0",
-  "52": "6.0", "53": "6.0", "54": "6.0", "55": "6.0", "56": "6.0",
-  "57": "7.0", "58": "7.0", "59": "7.0", "59": "7.0",
-  "60": "8.0", "61": "8.0", "62": "8.0", "63": "8.0",
-  "64": "9.0", "65": "9.0", "66": "9.0", "67": "9.0"
-}
-
 const create_webview_range = (value) => {
   return Number(value) < 37 ? "≤37" : value;
+}
+
+const getMatchingBrowserVersion = (dest_browser, source_browser_release) => {
+  var browserData = browsers[dest_browser];
+  for (var r in browserData.releases) {
+    var release = browserData.releases[r];
+    if (release.engine == source_browser_release.engine && Number(release.engine_version) >= Number(source_browser_release.engine_version)) {
+      return r;
+    }
+  }
+
+  return false;
 }
 
 const getSource = (browser) => {
@@ -109,43 +110,43 @@ const bumpVersion = (data, destination, source) => {
 
     else if (destination == 'opera') {
       if (typeof(data.version_added) === 'string') {
-        newValue.version_added = Math.max(15, Number(data.version_added) - 13).toString();
+        newValue.version_added = getMatchingBrowserVersion(destination, browsers[source].releases[data.version_added]);
       }
 
       if (data.version_removed) {
         if (typeof(data.version_removed) === 'string') {
-          newValue.version_removed = Math.max(15, Number(data.version_removed) - 13).toString();
+          newValue.version_removed = getMatchingBrowserVersion(destination, browsers[source].releases[data.version_removed]);
         }
       }
 
       if (typeof(data.notes) === 'string') {
         newValue.notes = newValue.notes.replace(/Chrome/g, "Opera");
       }
-  }
+    }
 
     else if (destination == 'opera_android') {
       if (typeof(data.version_added) === 'string') {
-        newValue.version_added = Math.max(14, Number(data.version_added) - 13).toString();
+        newValue.version_added = getMatchingBrowserVersion(destination, browsers[source].releases[data.version_added]);
       }
 
       if (data.version_removed) {
         if (typeof(data.version_removed) === 'string') {
-          newValue.version_removed = Math.max(14, Number(data.version_removed) - 13).toString();
+          newValue.version_removed = getMatchingBrowserVersion(destination, browsers[source].releases[data.version_removed]);
         }
       }
 
       if (typeof(data.notes) === 'string') {
         newValue.notes = newValue.notes.replace(/Chrome/g, "Opera");
       }
-  }
+    }
 
     else if (destination == 'samsunginternet_android') {
       if (newValue.version_added !== null) {
-        newValue.version_added = samsunginternet_mapping[newValue.version_added] || newValue.version_added;
+        newValue.version_added = getMatchingBrowserVersion(destination, browsers[source].releases[data.version_added]);
       }
 
       if (data.version_removed && newValue.version_removed != null) {
-        newValue.version_removed = samsunginternet_mapping[newValue.version_removed] || newValue.version_removed;
+        newValue.version_removed = getMatchingBrowserVersion(destination, browsers[source].releases[data.version_removed]);
       }
 
       if (typeof(data.notes) === 'string') {
