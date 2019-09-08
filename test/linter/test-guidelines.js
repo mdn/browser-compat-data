4f5b4b1a1d3a8807dd8ab9bb87023a1c614e1a07
Germain Souquet <germain@souquet.com>
2019-09-08 20:40:00 +0100

    create event linting rule and fix incorrect descriptions

--- test/linter/test-guidelines.js
+++ test/linter/test-guidelines.js
@@ -21,8 +21,22 @@ function hasValidConstrutorDescription(data, logger) {
  * @param {Identifier} data
  * @param {import('../utils').Logger} logger
  */
-function checkDOMEvents(data, logger) {
-
+function hasCorrectDOMEventsDescription(data, logger) {
+  if (data.api) {
+    for (let apiName in data.api) {
+      for (let methodName in data.api[apiName]) {
+        if (methodName.endsWith("_event")) {
+          const event = data.api[apiName][methodName];
+          const eventName = methodName.replace("_event", "");
+          if (event.__compat.description !== `<code>${eventName}</code> event`) {
+            logger.error(chalk`{red Incorrect event description for {bold ${apiName}#${methodName}}
+            Actual: "${event.__compat.description || ""}"
+            Expected: "<code>${eventName}</code> event"}`);
+          }
+        }
+      }
+    }
+  }
 }
 
 /**
@@ -59,6 +73,7 @@ function testGuidelines(filename) {
   };
 
   hasValidConstrutorDescription(data, logger);
+  hasCorrectDOMEventsDescription(data, logger);
 
   if (errors.length) {
     console.error(chalk`{red   Guidelines – {bold ${errors.length}} ${errors.length === 1 ? 'error' : 'errors'}:}`);
