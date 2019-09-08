c5b882595ff225c9de65c529d0bcdbb3bc51068c
Germain Souquet <germain@souquet.com>
2019-09-08 20:15:45 +0100

    create constructor linting rule and fix incorrect descriptions

--- test/linter/test-guidelines.js
+++ test/linter/test-guidelines.js
@@ -1,3 +1,47 @@
+const chalk = require('chalk');
+
+/**
+ * @param {Identifier} data
+ * @param {import('../utils').Logger} logger
+ */
+function hasValidConstrutorDescription(data, logger) {
+  if (data.api) {
+    for (let apiName in data.api) {
+      const constructor = data.api[apiName][apiName];
+      if (constructor && constructor.__compat.description !== `<code>${apiName}()</code> constructor`) {
+          logger.error(chalk`{red Incorrect constructor description for {bold ${apiName}()
+          Actual: "${constructor.__compat.description || ""}"
+          Expected: "<code>${apiName}()</code> constructor"}}`);
+      }
+    }
+  }
+}
+
+/**
+ * @param {Identifier} data
+ * @param {import('../utils').Logger} logger
+ */
+function checkDOMEvents(data, logger) {
+
+}
+
+/**
+ * @param {Identifier} data
+ * @param {import('../utils').Logger} logger
+ */
+function checkSecureContextRequired(data, logger) {
+
+}
+
+/**
+ * @param {Identifier} data
+ * @param {import('../utils').Logger} logger
+ */
+function checkWebWorkers(data, logger) {
+
+}
+
+
 /**
  * @param {string} filename
  */
@@ -13,6 +57,8 @@ function testGuidelines(filename) {
       errors.push(message.join(' '));
     },
   };
+
+  hasValidConstrutorDescription(data, logger);
 
   if (errors.length) {
     console.error(chalk`{red   Guidelines – {bold ${errors.length}} ${errors.length === 1 ? 'error' : 'errors'}:}`);
