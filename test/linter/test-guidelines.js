077df8e099cd7df260b7f9d2e6d88a029bf76f9b
Germain <germain@grabyo.com>
2019-09-09 13:33:13 +0100

    create data guidelines linting rules

---
+++ test/linter/test-guidelines.js
@@ -0,0 +1,108 @@
+const chalk = require('chalk');
+
+/**
+ * @param {Identifier} data
+ * @param {import('../utils').Logger} logger
+ */
+function hasValidConstrutorDescription(data, logger) {
+  if (data.api) {
+    for (let apiName in data.api) {
+      const constructor = data.api[apiName][apiName];
+      if (constructor && constructor.__compat.description !== `<code>${apiName}()</code> constructor`) {
+          logger.error(chalk`{red Incorrect constructor description for {bold ${apiName}()
+          Actual: "${constructor.__compat.description || ""}"
+          Expected: "<code>${apiName}()</code> constructor"}}`);
+      }
+    }
+  }
+}
+
+/**
+ * @param {Identifier} data
+ * @param {import('../utils').Logger} logger
+ */
+function hasCorrectDOMEventsDescription(data, logger) {
+  if (data.api) {
+    for (let apiName in data.api) {
+      for (let methodName in data.api[apiName]) {
+        if (methodName.endsWith("_event")) {
+          const event = data.api[apiName][methodName];
+          const eventName = methodName.replace("_event", "");
+          if (event.__compat.description !== `<code>${eventName}</code> event`) {
+            logger.error(chalk`{red Incorrect event description for {bold ${apiName}#${methodName}}
+            Actual: "${event.__compat.description || ""}"
+            Expected: "<code>${eventName}</code> event"}`);
+          }
+        }
+      }
+    }
+  }
+}
+
+/**
+ * @param {Identifier} data
+ * @param {import('../utils').Logger} logger
+ */
+function hasCorrectSecureContextRequiredDescription(data, logger) {
+  if (data.api) {
+    for (let apiName in data.api) {
+      const secureContext = data.api[apiName].secure_context_required;
+      if (secureContext && secureContext.__compat.description !== `Secure context required`) {
+          logger.error(chalk`{red Incorrect secure context required description for {bold ${apiName}()}
+          Actual: "${secureContext.__compat.description || ""}"
+          Expected: {bold "Secure context required"}}`);
+      }
+    }
+  }
+}
+
+/**
+ * @param {Identifier} data
+ * @param {import('../utils').Logger} logger
+ */
+function hasCorrectWebWorkersDescription(data, logger) {
+  if (data.api) {
+    for (let apiName in data.api) {
+      const workerSupport = data.api[apiName].worker_support;
+      if (workerSupport && workerSupport.__compat.description !== `Available in workers`) {
+          logger.error(chalk`{red Incorrect worker support description for {bold ${apiName}()}
+          Actual: "${workerSupport.__compat.description || ""}"
+          Expected: {bold "Available in workers"}}`);
+      }
+    }
+  }
+}
+
+
+/**
+ * @param {string} filename
+ */
+function testGuidelines(filename) {
+  /** @type {Identifier} */
+  const data = require(filename);
+
+  /** @type {string[]} */
+  const errors = [];
+  const logger = {
+    /** @param {...unknown} message */
+    error: (...message) => {
+      errors.push(message.join(' '));
+    },
+  };
+
+  hasValidConstrutorDescription(data, logger);
+  hasCorrectDOMEventsDescription(data, logger);
+  hasCorrectSecureContextRequiredDescription(data, logger);
+  hasCorrectWebWorkersDescription(data, logger);
+
+  if (errors.length) {
+    console.error(chalk`{red   Guidelines – {bold ${errors.length}} ${errors.length === 1 ? 'error' : 'errors'}:}`);
+    for (const error of errors) {
+      console.error(`    ${error}`);
+    }
+    return true;
+  }
+  return false;
+}
+
+module.exports = testGuidelines;
