962f9689f2ef5a25cc013f04c1645ffb74b926a2
Germain Souquet <germain@souquet.com>
2019-09-11 23:12:32 +0100

    make all linter checks over one data iteration

--- test/linter/test-guidelines.js
+++ test/linter/test-guidelines.js
@@ -1,75 +1,63 @@
 const chalk = require('chalk');
 
 /**
- * @param {Identifier} data
+ * @param {Identifier} apiData
+ * @param {String} apiName
  * @param {import('../utils').Logger} logger
  */
-function hasValidConstrutorDescription(data, logger) {
-  if (data.api) {
-    for (let apiName in data.api) {
-      const constructor = data.api[apiName][apiName];
-      if (constructor && constructor.__compat.description !== `<code>${apiName}()</code> constructor`) {
-          logger.error(chalk`{red Incorrect constructor description for {bold ${apiName}()
-          Actual: "${constructor.__compat.description || ""}"
-          Expected: "<code>${apiName}()</code> constructor"}}`);
+function hasValidConstrutorDescription(apiData, apiName, logger) {
+  const constructor = apiData[apiName];
+  if (constructor && constructor.__compat.description !== `<code>${apiName}()</code> constructor`) {
+      logger.error(chalk`{red Incorrect constructor description for {bold ${apiName}()
+      Actual: "${constructor.__compat.description || ""}"
+      Expected: "<code>${apiName}()</code> constructor"}}`);
+  }
+}
+
+/**
+ * @param {Identifier} apiData
+ * @param {String} apiName
+ * @param {import('../utils').Logger} logger
+ */
+function hasCorrectDOMEventsDescription(apiData, apiName, logger) {
+  for (let methodName in apiData) {
+    if (methodName.endsWith("_event")) {
+      const event = apiData[methodName];
+      const eventName = methodName.replace("_event", "");
+      if (event.__compat.description !== `<code>${eventName}</code> event`) {
+        logger.error(chalk`{red Incorrect event description for {bold ${apiName}#${methodName}}
+        Actual: "${event.__compat.description || ""}"
+        Expected: "<code>${eventName}</code> event"}`);
       }
     }
   }
 }
 
 /**
- * @param {Identifier} data
+ * @param {Identifier} apiData
+ * @param {String} apiName
  * @param {import('../utils').Logger} logger
  */
-function hasCorrectDOMEventsDescription(data, logger) {
-  if (data.api) {
-    for (let apiName in data.api) {
-      for (let methodName in data.api[apiName]) {
-        if (methodName.endsWith("_event")) {
-          const event = data.api[apiName][methodName];
-          const eventName = methodName.replace("_event", "");
-          if (event.__compat.description !== `<code>${eventName}</code> event`) {
-            logger.error(chalk`{red Incorrect event description for {bold ${apiName}#${methodName}}
-            Actual: "${event.__compat.description || ""}"
-            Expected: "<code>${eventName}</code> event"}`);
-          }
-        }
-      }
-    }
+function hasCorrectSecureContextRequiredDescription(apiData, apiName, logger) {
+  const secureContext = apiData.secure_context_required;
+  if (secureContext && secureContext.__compat.description !== `Secure context required`) {
+      logger.error(chalk`{red Incorrect secure context required description for {bold ${apiName}()}
+      Actual: "${secureContext.__compat.description || ""}"
+      Expected: {bold "Secure context required"}}`);
   }
 }
 
 /**
- * @param {Identifier} data
+ * @param {Identifier} apiData
+ * @param {String} apiName
  * @param {import('../utils').Logger} logger
  */
-function hasCorrectSecureContextRequiredDescription(data, logger) {
-  if (data.api) {
-    for (let apiName in data.api) {
-      const secureContext = data.api[apiName].secure_context_required;
-      if (secureContext && secureContext.__compat.description !== `Secure context required`) {
-          logger.error(chalk`{red Incorrect secure context required description for {bold ${apiName}()}
-          Actual: "${secureContext.__compat.description || ""}"
-          Expected: {bold "Secure context required"}}`);
-      }
-    }
-  }
-}
-
-/**
- * @param {Identifier} data
- * @param {import('../utils').Logger} logger
- */
-function hasCorrectWebWorkersDescription(data, logger) {
-  if (data.api) {
-    for (let apiName in data.api) {
-      const workerSupport = data.api[apiName].worker_support;
-      if (workerSupport && workerSupport.__compat.description !== `Available in workers`) {
-          logger.error(chalk`{red Incorrect worker support description for {bold ${apiName}()}
-          Actual: "${workerSupport.__compat.description || ""}"
-          Expected: {bold "Available in workers"}}`);
-      }
-    }
+function hasCorrectWebWorkersDescription(apiData, apiName, logger) {
+  const workerSupport = apiData.worker_support;
+  if (workerSupport && workerSupport.__compat.description !== `Available in workers`) {
+      logger.error(chalk`{red Incorrect worker support description for {bold ${apiName}()}
+      Actual: "${workerSupport.__compat.description || ""}"
+      Expected: {bold "Available in workers"}}`);
   }
 }
 
@@ -90,10 +78,15 @@ function testGuidelines(filename) {
     },
   };
 
-  hasValidConstrutorDescription(data, logger);
-  hasCorrectDOMEventsDescription(data, logger);
-  hasCorrectSecureContextRequiredDescription(data, logger);
-  hasCorrectWebWorkersDescription(data, logger);
+  if (data.api) {
+    for (let apiName in data.api) {
+      const apiData = data.api[apiName];
+      hasValidConstrutorDescription(apiData, apiName, logger);
+      hasCorrectDOMEventsDescription(apiData, apiName, logger);
+      hasCorrectSecureContextRequiredDescription(apiData, apiName, logger);
+      hasCorrectWebWorkersDescription(apiData, apiName, logger);
+    }
+  }
 
   if (errors.length) {
     console.error(chalk`{red   Guidelines – {bold ${errors.length}} ${errors.length === 1 ? 'error' : 'errors'}:}`);
