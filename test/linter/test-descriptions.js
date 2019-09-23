a2aabd09be97ad9532f9c2294cf245341268907a
Queen Vinyl Darkscratch <vinyldarkscratch@gmail.com>
2019-09-23 16:05:05 -0700

    Consolidate descriptions linter functions into one

--- test/linter/test-descriptions.js
+++ test/linter/test-descriptions.js
@@ -5,62 +5,37 @@ const chalk = require('chalk');
  * @param {String} apiName
  * @param {import('../utils').Logger} logger
  */
-function hasValidConstrutorDescription(apiData, apiName, logger) {
-  const constructor = apiData[apiName];
-  if (constructor && constructor.__compat.description !== `<code>${apiName}()</code> constructor`) {
-      logger.error(chalk`{red Incorrect constructor description for {bold ${apiName}()}
-      Actual: {yellow "${constructor.__compat.description || ""}"}
-      Expected: {green "<code>${apiName}()</code> constructor"}}`);
-  }
-}
-
-/**
- * @param {Identifier} apiData
- * @param {String} apiName
- * @param {import('../utils').Logger} logger
- */
-function hasCorrectDOMEventsDescription(apiData, apiName, logger) {
+function processData(apiData, apiName, logger) {
   for (let methodName in apiData) {
-    if (methodName.endsWith("_event")) {
-      const event = apiData[methodName];
+    const method = apiData[methodName];
+    if (methodName == apiName) {
+      if (method.__compat.description !== `<code>${apiName}()</code> constructor`) {
+        logger.error(chalk`{red Incorrect constructor description for {bold ${apiName}()}
+      Actual: {yellow "${method.__compat.description || ""}"}
+      Expected: {green "<code>${apiName}()</code> constructor"}}`);
+      }
+    } else if (methodName.endsWith("_event")) {
       const eventName = methodName.replace("_event", "");
-      if (event.__compat.description !== `<code>${eventName}</code> event`) {
-        logger.error(chalk`{red Incorrect event description for {bold ${apiName}#${methodName}}
-      Actual: {yellow "${event.__compat.description || ""}"}
+      if (method.__compat.description !== `<code>${eventName}</code> event`) {
+        logger.error(chalk`{red Incorrect event description for {bold ${apiName}.${methodName}}
+      Actual: {yellow "${method.__compat.description || ""}"}
       Expected: {green "<code>${eventName}</code> event"}}`);
+      }
+    } else if (methodName == 'secure_context_required') {
+      if (method.__compat.description !== `Secure context required`) {
+        logger.error(chalk`{red Incorrect secure context required description for {bold ${apiName}()}
+      Actual: {yellow "${method.__compat.description || ""}"}
+      Expected: {green "Secure context required"}}`);
+      }
+    } else if (methodName == 'worker_support') {
+      if (method.__compat.description !== `Available in workers`) {
+        logger.error(chalk`{red Incorrect worker support description for {bold ${apiName}()}
+      Actual: {yellow "${method.__compat.description || ""}"}
+      Expected: {green "Available in workers"}}`);
       }
     }
   }
 }
-
-/**
- * @param {Identifier} apiData
- * @param {String} apiName
- * @param {import('../utils').Logger} logger
- */
-function hasCorrectSecureContextRequiredDescription(apiData, apiName, logger) {
-  const secureContext = apiData.secure_context_required;
-  if (secureContext && secureContext.__compat.description !== `Secure context required`) {
-      logger.error(chalk`{red Incorrect secure context required description for {bold ${apiName}()}
-      Actual: {yellow "${secureContext.__compat.description || ""}"}
-      Expected: {green "Secure context required"}}`);
-  }
-}
-
-/**
- * @param {Identifier} apiData
- * @param {String} apiName
- * @param {import('../utils').Logger} logger
- */
-function hasCorrectWebWorkersDescription(apiData, apiName, logger) {
-  const workerSupport = apiData.worker_support;
-  if (workerSupport && workerSupport.__compat.description !== `Available in workers`) {
-      logger.error(chalk`{red Incorrect worker support description for {bold ${apiName}()}
-      Actual: {yellow "${workerSupport.__compat.description || ""}"}
-      Expected: {green "Available in workers"}}`);
-  }
-}
-
 
 /**
  * @param {string} filename
@@ -81,10 +56,7 @@ function testDescriptions(filename) {
   if (data.api) {
     for (let apiName in data.api) {
       const apiData = data.api[apiName];
-      hasValidConstrutorDescription(apiData, apiName, logger);
-      hasCorrectDOMEventsDescription(apiData, apiName, logger);
-      hasCorrectSecureContextRequiredDescription(apiData, apiName, logger);
-      hasCorrectWebWorkersDescription(apiData, apiName, logger);
+      processData(apiData, apiName, logger);
     }
   }
 
