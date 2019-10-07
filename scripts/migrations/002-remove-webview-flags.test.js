c11d8cf95c34d409f22a4ec9774d9050caa1c6db
Queen Vinyl Darkscratch <vinyldarkscratch@gmail.com>
2019-10-07 05:43:56 -0700

    Allow for multiple tests

--- scripts/migrations/002-remove-webview-flags.test.js
+++ scripts/migrations/002-remove-webview-flags.test.js
@@ -10,62 +10,97 @@ const { platform } = require('os');
 
 const { removeWebViewFlags } = require('./002-remove-webview-flags.js');
 
-const input = JSON.stringify({
-  "test": {
-    "__compat": {
-      "support": {
-        "webview_android": {
-          "version_added": "61",
-          "flags": [
-            {
-              "type": "preference",
-              "name": "#service-worker-payment-apps",
-              "value_to_set": "Enabled"
+const tests = [
+  [
+    {
+      "test": {
+        "__compat": {
+          "support": {
+            "webview_android": {
+              "version_added": "61",
+              "flags": [
+                {
+                  "type": "preference",
+                  "name": "#service-worker-payment-apps",
+                  "value_to_set": "Enabled"
+                }
+              ]
             }
-          ]
+          },
+          "status": {
+            "experimental": true,
+            "standard_track": false,
+            "deprecated": false
+          }
         }
-      },
-      "status": {
-        "experimental": true,
-        "standard_track": false,
-        "deprecated": false
+      }
+    },
+    {
+      "test": {
+        "__compat": {
+          "support": {
+            "webview_android": {
+              "version_added": false
+            }
+          },
+          "status": {
+            "experimental": true,
+            "standard_track": false,
+            "deprecated": false
+          }
+        }
       }
     }
-  }
-}, null, 2);
-
-const expected = JSON.stringify({
-  "test": {
-    "__compat": {
-      "support": {
-        "webview_android": {
-          "version_added": false
+  ],
+  [
+    {
+      "test2": {
+        "__compat": {
+          "support": {
+            "webview_android": {
+              "version_added": true
+            }
+          },
+          "status": {
+            "experimental": true,
+            "standard_track": false,
+            "deprecated": false
+          }
         }
-      },
-      "status": {
-        "experimental": true,
-        "standard_track": false,
-        "deprecated": false
+      }
+    },
+    {
+      "test2": {
+        "__compat": {
+          "support": {
+            "webview_android": {
+              "version_added": true
+            }
+          },
+          "status": {
+            "experimental": true,
+            "standard_track": false,
+            "deprecated": false
+          }
+        }
       }
     }
-  }
-}, null, 2);
-
-/** Determines if the OS is Windows */
-const IS_WINDOWS = platform() === 'win32';
+  ]
+];
 
 const testFixWebViewFlags = (logger = console) => {
-  let output = JSON.stringify(JSON.parse(input, removeWebViewFlags), null, 2);
+  var hasErrors = false;
+  for (let i = 0; i < tests.length; i++) {
+    let expected = JSON.stringify(tests[i][1], null, 2);
+    let output = JSON.stringify(JSON.parse(JSON.stringify(tests[i][0]), removeWebViewFlags), null, 2);
 
-  if (IS_WINDOWS) { // prevent false positives from git.core.autocrlf on Windows
-    output = output.replace(/\r/g, '');
+    if (output !== expected) {
+      logger.error(chalk`WebView flags aren't removed properly!\n      Actual: {yellow ${output}}\n      Expected: {green ${expected}}`);
+      hasErrors = true;
+    }
   }
 
-  if (output !== expected) {
-    logger.error(chalk`WebView flags aren't removed properly!\n      Actual: {yellow ${output}}\n      Expected: {green ${expected}}`);
-    return true;
-  }
-  return false;
+  return hasErrors;
 }
 
 if (require.main === module) {
