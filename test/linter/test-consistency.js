d66e287d8494fe9f717bb6857e3386fc009e3dce
ExE Boss <3889017+ExE-Boss@users.noreply.github.com>
2019-11-18 17:00:00 +0100

    test(lint): Revert string delimiter fixes to `test‑consistency.js`

--- test/linter/test-consistency.js
+++ test/linter/test-consistency.js
@@ -234,7 +234,7 @@ class ConsistencyChecker {
     if (compatData.constructor === Array) {
       for (var i = compatData.length - 1; i >= 0; i--) {
         var va = compatData[i].version_added;
-        if (typeof(va) === 'string' && (version_added == null || (typeof(version_added) === 'string' && compareVersions.compare(version_added.replace('≤', ''), va.replace('≤', ''), '>')))) {
+        if (typeof(va) === 'string' && (version_added == null || (typeof(version_added) === 'string' && compareVersions.compare(version_added.replace("≤", ""), va.replace("≤", ""), ">")))) {
           version_added = va;
         }
       }
@@ -253,10 +253,10 @@ class ConsistencyChecker {
     var b_version_added = this.getVersionAdded(b);
 
     if (typeof(a_version_added) === 'string' && typeof(b_version_added) === 'string') {
-      if (a_version_added.startsWith('≤') || b_version_added.startsWith('≤')) {
+      if (a_version_added.startsWith("≤") || b_version_added.startsWith("≤")) {
         return false;
       }
-      return compareVersions.compare(a_version_added, b_version_added, '<');
+      return compareVersions.compare(a_version_added, b_version_added, "<");
     }
 
     return false;
@@ -299,16 +299,16 @@ function testConsistency(filename) {
     errors.forEach(({ feature, path, errors }) =>  {
       console.error(chalk`{red   → {bold ${errors.length}} × {bold ${feature}} [${path.join('.')}]: }`);
       errors.forEach(({ errortype, browser, parent_value, subfeatures }) => {
-        if (errortype == 'unsupported') {
+        if (errortype == "unsupported") {
           console.error(chalk`{red     → No support in {bold ${browser}}, but support is declared in the following sub-feature(s):}`);
-        } else if (errortype == 'support_unknown') {
+        } else if (errortype == "support_unknown") {
           console.error(chalk`{red     → Unknown support in parent for {bold ${browser}}, but support is declared in the following sub-feature(s):}`);
-        } else if (errortype == 'subfeature_earlier_implementation') {
+        } else if (errortype == "subfeature_earlier_implementation") {
           console.error(chalk`{red     → Basic support in {bold ${browser}} was declared implemented in a later version ({bold ${parent_value}}) than the following sub-feature(s):}`);
         }
 
         subfeatures.forEach(subfeature => {
-          console.error(chalk`{red       → {bold ${path.join('.')}.${subfeature[0]}}: ${subfeature[1] || '[Array]'}}`);
+          console.error(chalk`{red       → {bold ${path.join('.')}.${subfeature[0]}}: ${subfeature[1] || "[Array]"}}`);
         });
       });
     });
