e391108c83a9106313bb98caee89ccf82d6bcec1
Queen Vinyl Darkscratch <vinyldarkscratch@gmail.com>
2019-11-20 23:02:16 -0800

    Refactor link tests

--- test/linter/test-links.js
+++ test/linter/test-links.js
@@ -8,7 +8,7 @@ const { IS_WINDOWS, indexToPos } = require('../utils.js');
  * @param {import('../utils').Logger} logger
  */
 function processData(filename, logger) {
-  let hasErrors = false;
+  let errors = [];
 
   let actual = fs.readFileSync(filename, 'utf-8').trim();
   /** @type {import('../../types').CompatData} */
@@ -21,42 +21,41 @@ function processData(filename, logger) {
     expected = expected.replace(/\r/g, '');
   }
 
-  /** @type {(regexp: string|RegExp, matchHandler: (match: RegExpMatchArray) => void) => void} */
-  const processLinks = PROCESS_LINKS.bind(this, actual);
-
-  processLinks(
+  processLink( // use https://bugzil.la/1000000 instead
+    errors,
+    actual,
     String.raw`https?://bugzilla\.mozilla\.org/show_bug\.cgi\?id=(\d+)`,
     match => {
-      // use https://bugzil.la/1000000 instead
-      hasErrors = true;
-      logger.error(chalk`{red → ${indexToPos(actual, match.index)} – Use shortenable URL ({yellow ${match[0]}} → {green {bold https://bugzil.la/}${match[1]}}).}`);
+      return {'issue': 'Use shortenable URL', 'expected': `https://bugzil.la/${match[1]}`};
     }
   );
 
-  processLinks(
+  processLink( // use https://crbug.com/100000 instead
+    errors,
+    actual,
     String.raw`https?://bugs\.chromium\.org/p/chromium/issues/detail\?id=(\d+)`,
     match => {
-      // use https://crbug.com/100000 instead
-      hasErrors = true;
-      logger.error(chalk`{red → ${indexToPos(actual, match.index)} – Use shortenable URL ({yellow ${match[0]}} → {green {bold https://crbug.com/}${match[1]}}).}`);
+      return {'issue': 'Use shortenable URL', 'expected': `https://crbug.com/${match[1]}`};
     }
   );
 
-  processLinks(
+  processLink( // use https://webkit.org/b/100000 instead
+    errors,
+    actual,
     String.raw`https?://bugs\.webkit\.org/show_bug\.cgi\?id=(\d+)`,
     match => {
-      // use https://webkit.org/b/100000 instead
-      hasErrors = true;
-      logger.error(chalk`{red → ${indexToPos(actual, match.index)} – Use shortenable URL ({yellow ${match[0]}} → {green {bold https://webkit.org/b/}${match[1]}}).}`);
+      return {'issue': 'Use shortenable URL', 'expected': `https://webkit.org/b/${match[1]}`};
     }
   );
 
-  processLinks(
-    // Bugzil.la links should use HTTPS and have "bug ###" as link text ("Bug ###" only at the begin of notes/sentences).
-    String.raw`(....)<a href='(https?)://(bugzil\.la|crbug\.com|webkit\.org/b)/(\d+)'>(.*?)</a>`,
+  processLink( // Bug links should use HTTPS and have "bug ###" as link text ("Bug ###" only at the begin of notes/sentences).
+    errors,
+    actual,
+    String.raw`(....)<a href='((https?)://(bugzil\.la|crbug\.com|webkit\.org/b)/(\d+))'>(.*?)</a>`,
     match => {
       const [,
         before,
+        url,
         protocol,
         domain,
         bugId,
@@ -64,30 +63,26 @@ function processData(filename, logger) {
       ] = match;
 
       if (protocol !== 'https') {
-        hasErrors = true;
-        logger.error(chalk`{red → ${indexToPos(actual, match.index)} – Use HTTPS URL ({yellow http://${domain}/${bugId}} → {green http{bold s}://${domain}/${bugId}}).}`);
+        return {'issue': 'Use HTTPS for bug links', 'expected': `https://${domain}/${bugId}`, 'actualLink': url};
       }
 
-      if (domain !== 'bugzil.la') {
-        return;
-      }
-
-      if (/^bug $/.test(before)) {
-        hasErrors = true;
-        logger.error(chalk`{red → ${indexToPos(actual, match.index)} – Move word "bug" into link text ({yellow "${before}<a href='...'>${linkText}</a>"} → {green "<a href='...'>{bold ${before}}${bugId}</a>"}).}`);
-      } else if (linkText === `Bug ${bugId}`) {
-        if (!/(\. |")$/.test(before)) {
-          hasErrors = true;
-          logger.error(chalk`{red → ${indexToPos(actual, match.index)} – Use lowercase "bug" word within sentence ({yellow "Bug ${bugId}"} → {green "{bold bug} ${bugId}"}).}`);
+      if (domain == 'bugzil.la') {
+        if (/^bug $/.test(before)) {
+          return {'issue': 'Move word "bug" into link text', 'expected': `<a href='...'>${before}${bugId}</a>`, 'actualLink': `${before}<a href='...'>${linkText}</a>`};
+        } else if (linkText === `Bug ${bugId}`) {
+          if (!/(\. |")$/.test(before)) {
+            return {'issue': 'Use lowercase "bug" word within sentence', 'expected': `bug ${bugId}`, 'actualLink': `Bug ${bugId}`};
+          }
+        } else if (linkText !== `bug ${bugId}`) {
+          return {'issue': 'Use standard link text', 'expected': `bug ${bugId}`, 'actualLink': linkText};
         }
-      } else if (linkText !== `bug ${bugId}`) {
-        hasErrors = true;
-        logger.error(chalk`{red → ${indexToPos(actual, match.index)} – Use standard link text ({yellow "${linkText}"} → {green "bug ${bugId}"}).}`);
       }
     }
   )
 
-  processLinks(
+  processLink(
+    errors,
+    actual,
     String.raw`\b(https?)://((?:[a-z][a-z0-9-]*\.)*)developer.mozilla.org/(.*?)(?=["'\s])`,
     match => {
       const [url, protocol, subdomain, path] = match;
@@ -116,55 +111,55 @@ function processData(filename, logger) {
         }
         expectedPath = 'docs/' + expectedPath;
       }
-      const pos = indexToPos(match.input, match.index);
 
       if (protocol !== 'https') {
-        hasErrors = true;
-        logger.error(
-          chalk`{red → ${pos} – Use HTTPS MDN URL ({yellow ${protocol}://developer.mozilla.org/${path}} → {green https://developer.mozilla.org/${expectedPath}}).}`,
-        );
+        return {'issue': 'Use HTTPS MDN URL', 'expected': `https://developer.mozilla.org/${expectedPath}`};
       }
 
       if (subdomain) {
-        hasErrors = true;
-        logger.error(
-          chalk`{red → ${pos} - Use correct MDN domain ({yellow ${protocol}://{red ${subdomain}}developer.mozilla.org/${path}} → {green https://developer.mozilla.org/${expectedPath}})}`,
-        );
+        return {'issue': 'Use correct MDN domain', 'expected': `https://developer.mozilla.org/${expectedPath}`};
       }
 
       if (path !== expectedPath) {
-        hasErrors = true;
-        logger.error(
-          chalk`{red → ${pos} – Use ${
-            locale ? 'non-localized' : 'correct'
-          } MDN URL ({yellow ${url}} → {green https://developer.mozilla.org/${expectedPath}}).}`,
-        );
+        return {'issue': `Use ${locale ? 'non-localized' : 'correct'} MDN URL`, 'expected': `https://developer.mozilla.org/${expectedPath}`};
       }
     }
   );
 
-  processLinks(
+  processLink(
+    errors,
+    actual,
     String.raw`https?://developer.microsoft.com/(\w\w-\w\w)/(.*?)(?=["'\s])`,
     match => {
-      hasErrors = true;
-      logger.error(chalk`{red → ${indexToPos(actual, match.index)} – Use non-localized Microsoft Developer URL ({yellow ${match[0]}} → {green https://developer.microsoft.com/${match[2]}}).}`);
+      return {'issue': 'Use non-localized Microsoft Developer URL', 'expected': `https://developer.microsoft.com/${match[2]}`};
     }
   );
 
-  return hasErrors;
+  errors.forEach((error, index) => {
+    logger.error(chalk`{red → ${error.pos} – ${error.issue} ({yellow ${error.actual}} → {green ${error.expected}}).}`);
+  })
+
+  return errors;
 }
 
 /**
+ * @param {Object[]} errors
  * @param {string} actual
  * @param {string|RegExp} regexp
- * @param {(match: RegExpExecArray) => void} matchHandler
+ * @param {(match: RegExpExecArray) => Object} matchHandler
  */
-const PROCESS_LINKS = function processLinks(actual, regexp, matchHandler) {
+function processLink(errors, actual, regexp, matchHandler) {
   const re = new RegExp(regexp, 'g');
   /** @type {RegExpExecArray} */
   let match;
   while ((match = re.exec(actual)) !== null) {
-    matchHandler(match);
+    let pos = indexToPos(actual, match.index);
+    let result = matchHandler(match);
+
+    if (result) {
+      let {issue, expected, actualLink = match[0]} = result;
+      errors.push({'issue': issue, 'pos': pos, 'actual': actualLink, 'expected': expected});
+    }
   }
 }
 
