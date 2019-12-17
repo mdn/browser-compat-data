7f9a9cba9c64626d59473a48eb6bb92935f53f7c
Queen Vinyl Darkscratch <vinyldarkscratch@gmail.com>
2019-12-17 00:10:17 -0800

    Switch to execSync and return if errors are present

--- test/test-format.js
+++ test/test-format.js
@@ -1,17 +1,22 @@
 'use strict';
-const { exec } = require('child_process');
+const { execSync } = require('child_process');
 const chalk = require('chalk');
 
 const testFormat = () => {
-  exec('npx prettier --check "**/*.js" "**/*.ts"', (error, stdout, stderr) => {
-    if (error) {
-      console.error(chalk`{red   Prettier – formatting errors:}`);
-      console.error(chalk`{red.bold ${stdout}}`);
-      console.error(
-        chalk`{blue Tip: Run {bold npm run fix} to fix formatting automatically}`,
-      );
-    }
-  });
+  try {
+    execSync('npx prettier --check "**/*.js" "**/*.ts"');
+  } catch (err) {
+    let errorText = err.stdout.toString();
+    console.error(chalk`{red   Prettier – formatting errors:}`);
+    console.error(chalk`{red.bold ${errorText}}`);
+    console.error(
+      chalk`{blue Tip: Run {bold npm run fix} to fix formatting automatically}`,
+    );
+
+    return true;
+  }
+
+  return false;
 };
 
 module.exports = testFormat;
