d9c1122e50e7c6a85ffe125e2c78b7d7a888396d
Queen Vinyl Darkscratch <vinyldarkscratch@gmail.com>
2020-01-10 04:39:46 -0800

    Remove redundant hasErrors variables from linters (#5188)

--- test/linter/test-real-values.js
+++ test/linter/test-real-values.js
@@ -58,7 +58,6 @@ const blockList = {
  * @param {Logger} logger
  */
 function checkRealValues(supportData, blockList, relPath, logger) {
-  let hasErrors = false;
   for (const browser of blockList) {
     /** @type {SimpleSupportStatement[]} */
     const supportStatements = [];
@@ -73,25 +72,20 @@ function checkRealValues(supportData, blockList, relPath, logger) {
         logger.error(
           chalk`{red → {bold ${browser}} must be defined for {bold ${relPath}}}`,
         );
-        hasErrors = true;
       } else {
         if ([true, null].includes(statement.version_added)) {
           logger.error(
             chalk`{red → {bold ${relPath}} - {bold ${browser}} no longer accepts {bold ${statement.version_added}} as a value}`,
           );
-          hasErrors = true;
         }
         if ([true, null].includes(statement.version_removed)) {
           logger.error(
             chalk`{red → {bold ${relPath}} - {bold ${browser}} no longer accepts} {bold ${statement.version_removed}} as a value}`,
           );
-          hasErrors = true;
         }
       }
     }
   }
-
-  return hasErrors;
 }
 
 /**
