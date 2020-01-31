f16844232173350120df0e1f351d4c94e11473eb
ExE Boss <3889017+ExE-Boss@users.noreply.github.com>
2020-01-31 06:00:00 +0100

    test: Fix JSDoc

--- test/test-compare-features.js
+++ test/test-compare-features.js
@@ -1,4 +1,3 @@
-#!/usr/bin/env node
 /* Any copyright is dedicated to the Public Domain.
  * http://creativecommons.org/publicdomain/zero/1.0/ */
 
--- test/test-regexes.js
+++ test/test-regexes.js
@@ -1,3 +1,7 @@
+#!/usr/bin/env node
+/* Any copyright is dedicated to the Public Domain.
+ * http://creativecommons.org/publicdomain/zero/1.0/ */
+
 'use strict';
 const assert = require('assert');
 
