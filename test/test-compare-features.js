23867c2a8796b45f855263025b4b99129e4c35a0
ExE Boss <3889017+ExE-Boss@users.noreply.github.com>
2020-01-31 06:00:00 +0100

    test: Cleanup imports

--- test/test-compare-features.js
+++ test/test-compare-features.js
@@ -3,8 +3,6 @@
  * http://creativecommons.org/publicdomain/zero/1.0/ */
 
 'use strict';
-const fs = require('fs');
-const path = require('path');
 const chalk = require('chalk');
 
 const compareFeatures = require('../scripts/compare-features');
