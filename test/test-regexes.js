ebeee1c06f933c4d8b985bf2c9cd398053fb8f7f
Queen Vinyl Darkscratch <vinyldarkscratch@gmail.com>
2020-02-05 10:33:05 +0100

    Add copyright notices

--- test/test-regexes.js
+++ test/test-regexes.js
@@ -1,3 +1,6 @@
+/* Any copyright is dedicated to the Public Domain.
+ * http://creativecommons.org/publicdomain/zero/1.0/ */
+
 'use strict';
 const assert = require('assert');
 const chalk = require('chalk');
--- test/test-utils.js
+++ test/test-utils.js
@@ -1,3 +1,6 @@
+/* Any copyright is dedicated to the Public Domain.
+ * http://creativecommons.org/publicdomain/zero/1.0/ */
+
 'use strict';
 const chalk = require('chalk');
 const { escapeInvisibles } = require('./utils.js');
