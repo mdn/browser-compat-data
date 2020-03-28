2165e07d77bc03e93109a32527bdfc2339b6401c
Queen Vinyl Darkscratch <vinyldarkscratch@gmail.com>
2020-03-28 10:49:09 -0700

    Include ESLint in test-format.js

--- test/test-format.js
+++ test/test-format.js
@@ -2,17 +2,30 @@
 const { execSync } = require('child_process');
 const chalk = require('chalk');
 
+/**
+ * @returns {boolean} If the file contains errors
+ */
 const testFormat = () => {
+  let errorText = '';
   try {
-    execSync('npx prettier --check "**/*.js" "**/*.ts" "**/*.md"', {
-      stdio: 'inherit',
-    });
+    execSync('npx eslint "**/*.js" --color');
   } catch (err) {
-    let errorText = err.stdout.toString();
-    console.error(chalk`{red   Prettier – formatting errors:}`);
+    errorText += err.stdout.toString();
+  }
+
+  try {
+    execSync('npx prettier "**/*.ts" "**/*.md" --color');
+  } catch (err) {
+    errorText += err.stdout.toString();
+  }
+
+  if (errorText !== '') {
+    console.error(
+      chalk`{red   Prettier/ESLint – code formatting/quality errors:}`,
+    );
     console.error(chalk`{red.bold ${errorText}}`);
     console.error(
-      chalk`{blue Tip: Run {bold npm run fix} to fix formatting automatically}`,
+      chalk`{blue Tip: Run {bold npm run fix} to fix some errors automatically}`,
     );
 
     return true;
