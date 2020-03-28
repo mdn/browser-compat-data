fa4b4477bda843b73a508925d84a4509140a8337
Queen Vinyl Darkscratch <vinyldarkscratch@gmail.com>
2020-03-28 11:52:27 -0700

    Inline processFilename

--- index.js
+++ index.js
@@ -7,31 +7,6 @@ const path = require('path');
 const extend = require('extend');
 
 /**
- * Process data from a file based upon the filename. If the given filename is a directory, recursively load it.
- *
- * @param {string} fn Filename to process
- * @returns {void}
- */
-const processFilename = fn => {
-  const fp = path.join(dir, fn);
-  let extra;
-
-  if (fs.statSync(fp).isDirectory()) {
-    extra = load(fp);
-  } else if (path.extname(fp) === '.json') {
-    try {
-      extra = require(fp);
-    } catch (e) {
-      console.error(`Error loading ${fp}: ${e}`);
-    }
-  }
-
-  // The JSON data is independent of the actual file
-  // hierarchy, so it is essential to extend "deeply".
-  result = extend(true, result, extra);
-};
-
-/**
  * Recursively load one or more files and/or directories passed as arguments.
  *
  * @param {string[]} files The files to test
@@ -43,7 +18,24 @@ const load = (...files) => {
 
   for (dir of files) {
     dir = path.resolve(__dirname, dir);
-    fs.readdirSync(dir).forEach(processFilename);
+    fs.readdirSync(dir).forEach(fn => {
+      const fp = path.join(dir, fn);
+      let extra;
+
+      if (fs.statSync(fp).isDirectory()) {
+        extra = load(fp);
+      } else if (path.extname(fp) === '.json') {
+        try {
+          extra = require(fp);
+        } catch (e) {
+          console.error(`Error loading ${fp}: ${e}`);
+        }
+      }
+
+      // The JSON data is independent of the actual file
+      // hierarchy, so it is essential to extend "deeply".
+      result = extend(true, result, extra);
+    });
   }
 
   return result;
