da4ba61d5f02da24d65da490d9a1115305d5a8d2
Queen Vinyl Darkscratch <vinyldarkscratch@gmail.com>
2020-03-28 12:17:14 -0700

    Move load functions to top level (and turn them into arrow functions)

--- scripts/fix-feature-order.js
+++ scripts/fix-feature-order.js
@@ -55,36 +55,36 @@ const fixFeatureOrder = filename => {
   }
 };
 
-if (require.main === module) {
-  /**
-   * @param {string[]} files
-   */
-  function load(...files) {
-    for (let file of files) {
-      if (file.indexOf(__dirname) !== 0) {
-        file = path.resolve(__dirname, '..', file);
-      }
-
-      if (!fs.existsSync(file)) {
-        continue; // Ignore non-existent files
-      }
-
-      if (fs.statSync(file).isFile()) {
-        if (path.extname(file) === '.json') {
-          fixFeatureOrder(file);
-        }
-
-        continue;
-      }
-
-      const subFiles = fs.readdirSync(file).map(subfile => {
-        return path.join(file, subfile);
-      });
-
-      load(...subFiles);
+/**
+ * @param {string[]} files
+ */
+const load = (...files) => {
+  for (let file of files) {
+    if (file.indexOf(__dirname) !== 0) {
+      file = path.resolve(__dirname, '..', file);
     }
-  }
 
+    if (!fs.existsSync(file)) {
+      continue; // Ignore non-existent files
+    }
+
+    if (fs.statSync(file).isFile()) {
+      if (path.extname(file) === '.json') {
+        fixFeatureOrder(file);
+      }
+
+      continue;
+    }
+
+    const subFiles = fs.readdirSync(file).map(subfile => {
+      return path.join(file, subfile);
+    });
+
+    load(...subFiles);
+  }
+};
+
+if (require.main === module) {
   if (process.argv[2]) {
     load(process.argv[2]);
   } else {
