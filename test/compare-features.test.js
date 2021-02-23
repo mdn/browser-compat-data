90ef8d3475da7bd7f648785ab87984e948eabfbd
Anton Bershanskiy <45960703+bershanskiy@users.noreply.github.com>
2021-02-23 12:01:52 +0300

    Run npm run fix

--- test/compare-features.test.js
+++ test/compare-features.test.js
@@ -13,52 +13,53 @@ const compareFeatures = require('../scripts/compare-features');
  */
 describe('compare-features script', () => {
   it('`compareFeatures()` works correctly', () => {
-  const input = [
-    'foobar',
-    'Foo',
-    '__compat',
-    'toString',
-    'secure_context_required',
-    'protocol-r30',
-    '$0',
-    'Bar',
-    '_updated_spec',
-    '43',
-    '--variable',
-    'ZOO_Pals',
-    '2-factor-auth',
-  ];
-  const actual = input.sort(compareFeatures);
-  const expected = [
-    '__compat',
-    'Bar',
-    'Foo',
-    'ZOO_Pals',
-    'foobar',
-    'protocol-r30',
-    'secure_context_required',
-    'toString',
-    '_updated_spec',
-    '--variable',
-    '$0',
-    '2-factor-auth',
-    '43',
-  ];
+    const input = [
+      'foobar',
+      'Foo',
+      '__compat',
+      'toString',
+      'secure_context_required',
+      'protocol-r30',
+      '$0',
+      'Bar',
+      '_updated_spec',
+      '43',
+      '--variable',
+      'ZOO_Pals',
+      '2-factor-auth',
+    ];
+    const actual = input.sort(compareFeatures);
+    const expected = [
+      '__compat',
+      'Bar',
+      'Foo',
+      'ZOO_Pals',
+      'foobar',
+      'protocol-r30',
+      'secure_context_required',
+      'toString',
+      '_updated_spec',
+      '--variable',
+      '$0',
+      '2-factor-auth',
+      '43',
+    ];
 
-  let errors = false;
-  for (let i = actual.length; i--; ) {
-    if (actual[i] !== expected[i]) {
-      errors = true;
-      break;
+    let errors = false;
+    for (let i = actual.length; i--; ) {
+      if (actual[i] !== expected[i]) {
+        errors = true;
+        break;
+      }
     }
-  }
 
-  if (errors) {
-    console.error(chalk`{red compareFeatures() – {bold 1} error:}`);
-    console.error(chalk`{red   → Actual and expected orders do not match}`);
-    console.error(chalk`{yellow     Actual: {bold ${actual}}}`);
-    console.error(chalk`{green     Expected: {bold ${expected}}}`);
-    return true;
-  }
-  return false;
-})});
+    if (errors) {
+      console.error(chalk`{red compareFeatures() – {bold 1} error:}`);
+      console.error(chalk`{red   → Actual and expected orders do not match}`);
+      console.error(chalk`{yellow     Actual: {bold ${actual}}}`);
+      console.error(chalk`{green     Expected: {bold ${expected}}}`);
+      return true;
+    }
+    return false;
+  });
+});
