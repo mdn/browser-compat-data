d478d9e2427c2a8d75fdf00b91bfd5f1fc633fa1
Daniel D. Beck <daniel@ddbeck.com>
2021-03-12 19:20:31 +0000

    Make BCD require wrapper work

--- utils/bcd.js
+++ utils/bcd.js
@@ -1,8 +1,17 @@
+let requireId = '..';
+
 if (process.env.BCD_UTILS_BCD_PACKAGE_PATH) {
-  console.warn('Warning: loading BCD in development mode');
+  console.warn('Warning: loading BCD from alternate path');
+  requireId = process.env.BCD_UTILS_BCD_PACKAGE_PATH;
 }
 
-const bcd = require(process.env.BCD_UTILS_BCD_PACKAGE_PATH ||
-  '@mdn/browser-compat-data');
+if (process.env.BCD_UTILS_REQUIRE_PACKAGE) {
+  // To facilitate further experimentation while also not exposing this as part
+  // of BCD's public API. With this, you can use `/utils` as a submodule.
+  console.warn('Warning: `require`-ing BCD');
+  requireId = '@mdn/browser-compat-data';
+}
+
+const bcd = require(requireId);
 
 module.exports = bcd;
