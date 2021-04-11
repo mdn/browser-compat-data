abfe33e7025657d985b7454e0e831b89469dab7b
Jonathan Neal <jonathantneal@hotmail.com>
2021-04-11 10:57:03 -0400

    Remove extend dependency, becoming a zero-dependency package

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 'use strict';
 const fs = require('fs');
 const path = require('path');
-const extend = require('extend');
 
 function load() {
   // Recursively load one or more directories passed as arguments.
@@ -23,7 +22,7 @@ function load() {
 
     // The JSON data is independent of the actual file
     // hierarchy, so it is essential to extend "deeply".
-    result = extend(true, result, extra);
+    result = extend(result, extra);
   }
 
   for (dir of arguments) {
@@ -32,6 +31,29 @@ function load() {
   }
 
   return result;
+}
+
+function extend(a, b) {
+  // iterate over all direct and inherited enumerable properties
+  for (const name in b) {
+    let newValue = b[name];
+
+    // check if the new value is an object and its property exists on the former
+    if (typeof newValue === 'object' && newValue !== null && name in a) {
+      const oldValue = a[name];
+
+      // check if the former value is also an object
+      if (typeof oldValue === 'object' && oldValue !== null) {
+        // update the new value as a new object extended by the former and later
+        newValue = extend(extend({}, oldValue), newValue);
+      }
+    }
+
+    // update the object with the new value
+    a[name] = newValue;
+  }
+
+  return a;
 }
 
 module.exports = load(
