713c56f2d9f349cf6730af6de03bd44180fdd3fa
Anton Bershanskiy <45960703+bershanskiy@users.noreply.github.com>
2021-04-14 14:52:47 +0300

    Add Mocha test for bundling script

---
+++ scripts/release-build.test.js
@@ -0,0 +1,15 @@
+'use strict';
+const assert = require('assert');
+const { execSync } = require('child_process');
+
+const prebuiltPath = '../build';
+
+describe('release-build', () => {
+  it('pre-built bundles are identical to the source', done => {
+    execSync('npm run release-build');
+    const regular = require('../');
+    const bundled = require(prebuiltPath);
+    assert.deepEqual(regular, bundled);
+    done();
+  }).timeout(5000);
+});
