5704612e3b103b9aa6fd9bbba3d6972c225a5323
Anton Bershanskiy <45960703+bershanskiy@users.noreply.github.com>
2021-04-15 12:38:57 +0300

    Review feedback

--- scripts/release-build.test.js
+++ scripts/release-build.test.js
@@ -5,11 +5,10 @@ const { execSync } = require('child_process');
 const prebuiltPath = '../build';
 
 describe('release-build', () => {
-  it('pre-built bundles are identical to the source', done => {
+  it('pre-built bundles are identical to the source', () => {
     execSync('npm run release-build');
-    const regular = require('../');
+    const regular = require('..');
     const bundled = require(prebuiltPath);
     assert.deepEqual(regular, bundled);
-    done();
   }).timeout(5000); // Timeout must be long enough for all the file I/O
 });
