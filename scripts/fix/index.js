4a1b2d02fdbbf5c09a790d0d3b0dbf8161b631f9
Queen Vinyl Darkscratch <vinyldarkscratch@gmail.com>
2021-08-25 07:49:34 -0700

    Remove "fix-" prefix from fix files

--- scripts/fix/index.js
+++ scripts/fix/index.js
@@ -1,9 +1,9 @@
 'use strict';
 const fs = require('fs');
 const path = require('path');
-const fixBrowserOrder = require('./fix-browser-order');
-const fixFeatureOrder = require('./fix-feature-order');
-const format = require('./fix-format');
+const fixBrowserOrder = require('./browser-order');
+const fixFeatureOrder = require('./feature-order');
+const format = require('./format');
 
 /**
  * @param {string[]} files
