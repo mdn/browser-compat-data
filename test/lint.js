a4b6eed10ad30aa719d5acb819ba21210510c9d5
Anton Bershanskiy <45960703+bershanskiy@users.noreply.github.com>
2022-04-19 16:11:54 +0300

    Refactor: move migration script test out of linter (#9230)
    * Refactor: remove testMigrations out of linter (bad indentation)
    
    * Run npm run fix
    
    * Feedback
    
    Co-authored-by: Anton Bershanskiy <8knots@protonmail.com>

--- test/lint.js
+++ test/lint.js
@@ -16,7 +16,6 @@ const {
   testDescriptions,
 } = require('./linter/index.js');
 const { IS_CI } = require('./utils.js');
-const testMigrations = require('./test-migrations');
 const testFormat = require('./test-format');
 
 /** @type {Map<string, string>} */
@@ -155,7 +154,6 @@ var hasErrors = argv.files
       'webdriver',
       'webextensions',
     );
-hasErrors = testMigrations() || hasErrors;
 hasErrors = testFormat() || hasErrors;
 
 if (hasErrors) {
