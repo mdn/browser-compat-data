b621b50006d0dce36714186e394dcaa3cabb3af4
Queen Vinyl Da.i'gyu-Kazotetsu <vinyldarkscratch@gmail.com>
2022-04-20 10:44:59 -0700

    Switch copyright notice

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
-/* Any copyright is dedicated to the Public Domain.
- * http://creativecommons.org/publicdomain/zero/1.0/ */
+/* This file is a part of @mdn/browser-compat-data
+ * See LICENSE file for more information. */
 
 'use strict';
+
 const fs = require('fs');
 const path = require('path');
 
