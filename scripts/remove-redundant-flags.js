0a95f7656b9242296bc2ade459daa82827547b78
Queen Vinyl Da.i'gyu-Kazotetsu <vinyldarkscratch@gmail.com>
2022-04-20 07:24:57 -0700

    Remove shebang

--- scripts/remove-redundant-flags.js
+++ scripts/remove-redundant-flags.js
@@ -1,8 +1,8 @@
-#!/usr/bin/env node
 /* Any copyright is dedicated to the Public Domain.
  * http://creativecommons.org/publicdomain/zero/1.0/ */
 
 'use strict';
+
 const fs = require('fs');
 const path = require('path');
 const { platform } = require('os');
