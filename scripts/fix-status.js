d1f0a5c9cf4f4affcd2a02257bb84f0e9e05884d
Queen Vinyl Da.i'gyu-Kazotetsu <vinyldarkscratch@gmail.com>
2022-04-24 10:28:21 -0700

    Update JSDoc

--- scripts/fix-status.js
+++ scripts/fix-status.js
@@ -1,19 +1,6 @@
 /* Any copyright is dedicated to the Public Domain.
  * http://creativecommons.org/publicdomain/zero/1.0/ */
 
-/**
- * Return a new feature object whose first-level properties have been
- * ordered according to Array.prototype.sort, and so will be
- * stringified in that order as well. This relies on guaranteed "own"
- * property ordering, which is insertion order for non-integer keys
- * (which is our case).
- *
- * @param {string} key The key in the object
- * @param {*} value The value of the key
- *
- * @returns {*} The new value
- */
-
 'use strict';
 
 const fs = require('fs');
@@ -23,6 +10,14 @@ const { platform } = require('os');
 /** Determines if the OS is Windows */
 const IS_WINDOWS = platform() === 'win32';
 
+/**
+ * Return a new feature object whose status properties have been adjusted according to a few predefined rules.
+ *
+ * @param {string} key The key in the object
+ * @param {*} value The value of the key
+ *
+ * @returns {*} The new value
+ */
 const fixStatus = (key, value) => {
   const status = value?.__compat?.status;
   if (status && status.experimental && status.deprecated) {
