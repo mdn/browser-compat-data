b17827cafeb54082905d832feedc62111bbee9fb
Queen Vinyl Da.i'gyu-Kazotetsu <vinyldarkscratch@gmail.com>
2022-04-26 09:32:33 -0700

    Skip over a failed PR features diff in release notes

--- scripts/release-notes.js
+++ scripts/release-notes.js
@@ -20,7 +20,14 @@ function main(argv) {
   for (const pull of pullsFromGitHub(startVersionTag, endVersionTag)) {
     process.stderr.write(`Diffing features for #${pull.number}`);
 
-    const diff = diffFeatures({ ref1: pull.mergeCommit });
+    try {
+      const diff = diffFeatures({ ref1: pull.mergeCommit });
+    } catch (e) {
+      console.error(
+        `${e}\n (Failed to diff features for #${pull.number}, skipping)`,
+      );
+      continue;
+    }
 
     console.error(
       ` (${diff.added.length} added, ${diff.removed.length} removed)`,
