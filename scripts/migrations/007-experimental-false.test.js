a6a6833b72c42073b7bc998c3484c49dadc13013
Philip Jägenstedt <philip@foolip.org>
2022-05-11 09:11:20 +0200

    Fix/update tests

--- scripts/migrations/007-experimental-false.test.js
+++ scripts/migrations/007-experimental-false.test.js
@@ -64,6 +64,72 @@ describe('fixExperimental()', () => {
     };
     assert.equal(bcd.api.fetch.__compat.status.experimental, true);
     fixExperimental(bcd);
+    assert.equal(bcd.api.fetch.__compat.status.experimental, false);
+  });
+
+  it('just chrome', () => {
+    const bcd = {
+      api: {
+        fetch: {
+          __compat: {
+            support: {
+              chrome: {
+                version_added: '100',
+              },
+              firefox: {
+                version_added: false,
+              },
+              safari: {
+                version_added: false,
+              },
+            },
+            status: {
+              experimental: true,
+              standard_track: true,
+              deprecated: false,
+            },
+          },
+        },
+      },
+    };
     assert.equal(bcd.api.fetch.__compat.status.experimental, true);
+    fixExperimental(bcd);
+    assert.equal(bcd.api.fetch.__compat.status.experimental, true);
+  });
+
+  it('mobile chrome + safari', () => {
+    const bcd = {
+      api: {
+        fetch: {
+          __compat: {
+            support: {
+              chrome: {
+                version_added: false,
+              },
+              chrome_android: {
+                version_added: '100',
+              },
+              firefox: {
+                version_added: false,
+              },
+              safari: {
+                version_added: false,
+              },
+              safari_ios: {
+                version_added: '15',
+              },
+            },
+            status: {
+              experimental: true,
+              standard_track: true,
+              deprecated: false,
+            },
+          },
+        },
+      },
+    };
+    assert.equal(bcd.api.fetch.__compat.status.experimental, true);
+    fixExperimental(bcd);
+    assert.equal(bcd.api.fetch.__compat.status.experimental, false);
   });
 });
