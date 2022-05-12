c10b99f3077b6d4253916639807f199e23cc6ba6
Queen Vinyl Da.i'gyu-Kazotetsu <vinyldarkscratch@gmail.com>
2022-05-12 14:02:21 -0700

    Fix ESLint issues in migration 002

--- scripts/migrations/002-remove-webview-flags.js
+++ scripts/migrations/002-remove-webview-flags.js
@@ -40,8 +40,8 @@ const removeWebViewFlags = (key, value) => {
  * @param {string} filename
  */
 const fixWebViewFlags = (filename) => {
-  const actual = fs.readFileSync(filename, 'utf-8').trim();
-  const expected = JSON.stringify(
+  let actual = fs.readFileSync(filename, 'utf-8').trim();
+  let expected = JSON.stringify(
     JSON.parse(actual, removeWebViewFlags),
     null,
     2,
@@ -58,36 +58,36 @@ const fixWebViewFlags = (filename) => {
   }
 };
 
-if (require.main === module) {
-  /**
-   * @param {string[]} files
-   */
-  function load(...files) {
-    for (let file of files) {
-      if (file.indexOf(__dirname) !== 0) {
-        file = path.resolve(__dirname, '..', '..', file);
-      }
-
-      if (!fs.existsSync(file)) {
-        continue; // Ignore non-existent files
-      }
-
-      if (fs.statSync(file).isFile()) {
-        if (path.extname(file) === '.json') {
-          fixWebViewFlags(file);
-        }
-
-        continue;
-      }
-
-      const subFiles = fs.readdirSync(file).map((subfile) => {
-        return path.join(file, subfile);
-      });
-
-      load(...subFiles);
+/**
+ * @param {string[]} files
+ */
+function load(...files) {
+  for (let file of files) {
+    if (file.indexOf(__dirname) !== 0) {
+      file = path.resolve(__dirname, '..', '..', file);
     }
-  }
 
+    if (!fs.existsSync(file)) {
+      continue; // Ignore non-existent files
+    }
+
+    if (fs.statSync(file).isFile()) {
+      if (path.extname(file) === '.json') {
+        fixWebViewFlags(file);
+      }
+
+      continue;
+    }
+
+    const subFiles = fs.readdirSync(file).map((subfile) => {
+      return path.join(file, subfile);
+    });
+
+    load(...subFiles);
+  }
+}
+
+if (require.main === module) {
   if (process.argv[2]) {
     load(process.argv[2]);
   } else {
