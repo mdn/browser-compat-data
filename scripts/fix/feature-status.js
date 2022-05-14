2cfc567e86c48f866f68fe5ce2d5868d0499ad4c
Queen Vinyl Da.i'gyu-Kazotetsu <vinyldarkscratch@gmail.com>
2022-05-13 20:44:18 -0700

    Update fix script to include migration's code

--- scripts/fix/feature-status.js
+++ scripts/fix/feature-status.js
@@ -19,9 +19,50 @@ const { IS_WINDOWS } = require('../../test/utils.js');
  */
 const fixStatus = (key, value) => {
   const compat = value?.__compat;
-  if (compat && compat.spec_url && compat.status.standard_track === false) {
-    compat.status.standard_track = true;
+  if (compat) {
+    if (compat.spec_url && compat.status.standard_track === false) {
+      compat.status.standard_track = true;
+    }
+    if (compat.status.experimental) {
+      const browserSupport = new Set();
+
+      for (const [browser, support] of Object.entries(compat.support)) {
+        // Consider only the first part of an array statement.
+        const statement = Array.isArray(support) ? support[0] : support;
+        // Ignore anything behind flag, prefix or alternative name
+        if (statement.flags || statement.prefix || statement.alternative_name) {
+          continue;
+        }
+        if (statement.version_added && !statement.version_removed) {
+          browserSupport.add(browser);
+        }
+      }
+
+      // Now check which of Blink, Gecko and WebKit support it.
+
+      const engineSupport = new Set();
+
+      for (const browser of browserSupport) {
+        const currentRelease = Object.values(browsers[browser].releases).find(
+          (r) => r.status === 'current',
+        );
+        const engine = currentRelease.engine;
+        engineSupport.add(engine);
+      }
+
+      let engineCount = 0;
+      for (const engine of ['Blink', 'Gecko', 'WebKit']) {
+        if (engineSupport.has(engine)) {
+          engineCount++;
+        }
+      }
+
+      if (engineCount > 1) {
+        compat.status.experimental = false;
+      }
+    }
   }
+
   return value;
 };
 
