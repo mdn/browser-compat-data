05ed1b3cde021b77dddf9afe62fb399379baab97
Kagami Sascha Rosylight <saschanaz@outlook.com>
2022-05-16 00:33:50 +0200

    import fix 2

--- test/linter/index.js
+++ test/linter/index.js
@@ -14,7 +14,7 @@ const testRealValues = require('./test-real-values.js');
 const testSchema = require('./test-schema.js');
 const testStyle = require('./test-style.js');
 const testVersions = require('./test-versions.js');
-const testSupportHistory = require('./test-support-history.js');
+const { testSupportHistory } = require('./test-support-history.js');
 
 module.exports = {
   testBrowsersData,
