c17c33ef12b63060af2801e8df5e28f728174b8a
Kagami Sascha Rosylight <saschanaz@outlook.com>
2022-05-16 14:02:23 +0200

    Fix lint issue

--- scripts/fix/status-inheritance.js
+++ scripts/fix/status-inheritance.js
@@ -1,23 +1,8 @@
-#!/usr/bin/env node
-/* Any copyright is dedicated to the Public Domain.
- * http://creativecommons.org/publicdomain/zero/1.0/ */
-
-/**
- * Return a new feature object whose first-level properties have been
- * ordered according to Array.prototype.sort, and so will be
- * stringified in that order as well. This relies on guaranteed "own"
- * property ordering, which is insertion order for non-integer keys
- * (which is our case).
- *
- * @param {string} key The key in the object
- * @param {*} value The value of the key
- *
- * @returns {*} The new value
- */
+/* This file is a part of @mdn/browser-compat-data
+ * See LICENSE file for more information. */
 
 'use strict';
 const fs = require('fs');
-const path = require('path');
 const { platform } = require('os');
 
 /** Determines if the OS is Windows */
@@ -60,53 +45,5 @@ const fixStatusInheritance = (filename) => {
     fs.writeFileSync(filename, expected + '\n', 'utf-8');
   }
 };
-
-if (require.main === module) {
-  /**
-   * @param {string[]} files
-   */
-  function load(...files) {
-    for (let file of files) {
-      if (file.indexOf(__dirname) !== 0) {
-        file = path.resolve(__dirname, '..', file);
-      }
-
-      if (!fs.existsSync(file)) {
-        continue; // Ignore non-existent files
-      }
-
-      if (fs.statSync(file).isFile()) {
-        if (path.extname(file) === '.json') {
-          fixStatusInheritance(file);
-        }
-
-        continue;
-      }
-
-      const subFiles = fs.readdirSync(file).map((subfile) => {
-        return path.join(file, subfile);
-      });
-
-      load(...subFiles);
-    }
-  }
-
-  if (process.argv[2]) {
-    load(process.argv[2]);
-  } else {
-    load(
-      'api',
-      'css',
-      'html',
-      'http',
-      'svg',
-      'javascript',
-      'mathml',
-      'test',
-      'webdriver',
-      'webextensions',
-    );
-  }
-}
 
 module.exports = fixStatusInheritance;
