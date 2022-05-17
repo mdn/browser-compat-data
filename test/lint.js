728680eed4ac2011f4a10001b84321c0e72788e6
Queen Vinyl Da.i'gyu-Kazotetsu <vinyldarkscratch@gmail.com>
2022-05-17 02:43:04 -0700

    Remove redundant `hasErrors` variables

--- test/lint.js
+++ test/lint.js
@@ -54,84 +54,81 @@ let filesWithErrors = 0;
  * @returns {boolean} Whether any of the files passed have errors
  */
 const checkFiles = (...files) => {
-  return files.reduce((prevHasErrors, file) => {
+  for (let file of files) {
     if (file.indexOf(__dirname) !== 0) {
       file = path.resolve(__dirname, '..', file);
     }
 
     if (!fs.existsSync(file)) {
       console.warn(chalk`{yellow File {bold ${file}} doesn't exist!}`);
-      return prevHasErrors; // Ignore non-existent files
+      return;
     }
 
-    if (fs.statSync(file).isFile()) {
-      let fileHasErrors = false;
+    if (fs.statSync(file).isFile() && path.extname(file) === '.json') {
+      const relativeFilePath = path.relative(process.cwd(), file);
 
-      if (path.extname(file) === '.json') {
-        const relativeFilePath = path.relative(process.cwd(), file);
+      spinner.text = relativeFilePath;
 
-        spinner.text = relativeFilePath;
-
-        if (!IS_CI) {
-          // Continuous integration environments don't allow overwriting
-          // previous lines using VT escape sequences, which is how
-          // the spinner animation is implemented.
-          spinner.start();
-        }
-
-        let fileErrors = [];
-
-        const console_error = console.error;
-        console.error = (...args) => {
-          if (!fileHasErrors) {
-            fileHasErrors = true;
-            spinner['stream'] = process.stderr;
-            spinner.fail(chalk.red.bold(relativeFilePath));
-          }
-          console_error(...args);
-          fileErrors.push(...args);
-        };
-
-        try {
-          if (file.indexOf('browsers' + path.sep) !== -1) {
-            testSchema(file, './../../schemas/browsers.schema.json');
-            testLinks(file);
-            testBrowsersData(file);
-          } else {
-            testBrowsersPresence(file);
-            testConsistency(file);
-            testDescriptions(file);
-            testLinks(file);
-            testPrefix(file);
-            testRealValues(file);
-            testSchema(file);
-            testStyle(file);
-            testVersions(file);
-            testNotes(file);
-          }
-        } catch (e) {
-          console.error(e);
-        }
-
-        if (fileHasErrors) {
-          errors.push(chalk`{red.bold ✖ ${relativeFilePath}}`);
-          errors.push(...fileErrors);
-          filesWithErrors++;
-        } else {
-          console.error = console_error;
-          spinner.succeed();
-        }
+      if (!IS_CI) {
+        // Continuous integration environments don't allow overwriting
+        // previous lines using VT escape sequences, which is how
+        // the spinner animation is implemented.
+        spinner.start();
       }
 
-      return prevHasErrors || fileHasErrors;
+      let fileErrors = [];
+
+      const console_error = console.error;
+      console.error = (...args) => {
+        if (fileErrors.length === 0) {
+          // Set spinner to failure when first error is found
+          // Setting on every error causes duplicate output
+          spinner['stream'] = process.stderr;
+          spinner.fail(chalk.red.bold(relativeFilePath));
+        }
+        console_error(...args);
+        fileErrors.push(...args);
+      };
+
+      try {
+        if (file.indexOf('browsers' + path.sep) !== -1) {
+          testSchema(file, './../../schemas/browsers.schema.json');
+          testLinks(file);
+          testBrowsersData(file);
+        } else {
+          testBrowsersPresence(file);
+          testConsistency(file);
+          testDescriptions(file);
+          testLinks(file);
+          testPrefix(file);
+          testRealValues(file);
+          testSchema(file);
+          testStyle(file);
+          testVersions(file);
+          testNotes(file);
+        }
+      } catch (e) {
+        console.error(e);
+      }
+
+      if (fileErrors.length) {
+        errors.push(chalk`{red.bold ✖ ${relativeFilePath}}`);
+        errors.push(...fileErrors);
+        filesWithErrors++;
+      } else {
+        console.error = console_error;
+        spinner.succeed();
+      }
     }
 
-    const subFiles = fs.readdirSync(file).map((subfile) => {
-      return path.join(file, subfile);
-    });
+    if (fs.statSync(file).isDirectory()) {
+      const subFiles = fs.readdirSync(file).map((subfile) => {
+        return path.join(file, subfile);
+      });
 
-    return checkFiles(...subFiles) || prevHasErrors;
-  }, false);
+      checkFiles(...subFiles);
+    }
+  }
 };
 
 /**
@@ -141,13 +138,10 @@ const checkFiles = (...files) => {
  * @returns {boolean} Whether there were any errors
  */
 const main = (files) => {
-  /** @type {boolean} */
-  var hasErrors = false;
-
   if (files) {
-    hasErrors = checkFiles(...files);
+    checkFiles(...files);
   } else {
-    hasErrors = checkFiles(
+    checkFiles(
       'api',
       'browsers',
       'css',
@@ -161,7 +155,7 @@ const main = (files) => {
     );
   }
 
-  if (hasErrors) {
+  if (errors.length) {
     console.error('');
     console.error(
       chalk`{red Problems in {bold ${pluralize('file', filesWithErrors)}}:}`,
@@ -171,7 +165,7 @@ const main = (files) => {
     }
   }
 
-  return hasErrors;
+  return !!errors.length;
 };
 
 if (require.main === module) {
