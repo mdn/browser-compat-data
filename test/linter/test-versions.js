726ec210e722eacf264c998f1a4e4c06c8f1c497
Queen Vinyl Da.i'gyu-Kazotetsu <vinyldarkscratch@gmail.com>
2022-05-16 18:46:20 -0700

    Remove duplicate code

--- test/linter/test-versions.js
+++ test/linter/test-versions.js
@@ -168,19 +168,6 @@ function checkVersions(supportData, relPath, logger) {
         const statementKeys = Object.keys(statement);
 
         for (const property of ['version_added', 'version_removed']) {
-          if (!isValidVersion(browser, statement[property])) {
-            logger.error(
-              chalk`{red → {bold ${relPath}} - {bold ${property}: "${
-                statement[property]
-              }"} is {bold NOT} a valid version number for {bold ${browser}}\n    Valid {bold ${browser}} versions are: ${validBrowserVersions[
-                browser
-              ].join(', ')}}`,
-              browserTips[browser],
-            );
-          }
-        }
-
-        for (const property of ['version_added', 'version_removed']) {
           if (
             property == 'version_removed' &&
             statement.version_removed === undefined
