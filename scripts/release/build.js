b58c99a1e4109aa4a9bd5f84343a0cb164e652af
Queen Vinyl Da.i'gyu-Kazotetsu <vinyldarkscratch@gmail.com>
2022-05-17 23:28:23 -0700

    Add comment to wrapper file

--- scripts/release/build.js
+++ scripts/release/build.js
@@ -29,6 +29,8 @@ async function writeData() {
 async function writeWrapper() {
   const dest = path.resolve(directory, 'nodewrapper.mjs');
   const content = `import fs from 'node:fs';
+  const content = `// A small wrapper to allow ESM imports on older NodeJS versions that don't support import assertions
+import fs from 'node:fs';
 const bcd = JSON.parse(fs.readFileSync(new URL('./data.json', import.meta.url)));
 export default bcd;
 `;
