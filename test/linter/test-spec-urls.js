be54d3aae35b406c00b6f6b5d98d6ababd261199
Queen Vinyl Da.i'gyu-Kazotetsu <vinyldarkscratch@gmail.com>
2022-06-03 00:53:06 -0700

    Remove commented code

--- test/linter/test-spec-urls.js
+++ test/linter/test-spec-urls.js
@@ -81,47 +81,6 @@ function processData(data, logger) {
       );
     }
   }
-
-  //   let prefixes = [];
-  //
-  //   if (category === 'api') {
-  //     prefixes = [
-  //       'moz',
-  //       'Moz',
-  //       'MOZ_',
-  //       'webkit',
-  //       'WebKit',
-  //       'webKit',
-  //       'WEBKIT_',
-  //       'ms',
-  //       'MS',
-  //       'o',
-  //       'O',
-  //     ];
-  //   }
-  //   if (category === 'css') {
-  //     prefixes = ['-webkit-', '-moz-', '-ms-', '-o-', '-khtml-'];
-  //   }
-  //
-  //   if (prefixes.length === 0) {
-  //     // Prefixes aren't enforced for other categories
-  //     return;
-  //   }
-  //
-  //   for (const support of Object.values(data.support)) {
-  //     const supportStatements = Array.isArray(support) ? support : [support];
-  //
-  //     for (const statement of supportStatements) {
-  //       if (
-  //         statement.prefix &&
-  //         !prefixes.some((p) => statement.prefix.startsWith(p))
-  //       ) {
-  //         logger.error(
-  //           chalk`Prefix is set to {bold ${statement.prefix}}, which is invalid for ${category}`,
-  //         );
-  //       }
-  //     }
-  //   }
 }
 
 export default {
