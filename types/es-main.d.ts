ad35bc8fdd951c47c88e3b940c1ca23ef7edb75d
Queen Vinyl Da.i'gyu-Kazotetsu <vinyldarkscratch@gmail.com>
2022-06-07 10:12:15 -0700

    Update internal TypeScript definitons

--- types/es-main.d.ts
+++ types/es-main.d.ts
@@ -1,14 +1,6 @@
 /* This file is a part of @mdn/browser-compat-data
  * See LICENSE file for more information. */
 
-import { CompatData } from './types';
-
-export default CompatData;
-
-export type InternalSupportStatement = SupportStatement | 'mirror';
-
-// External Module Declarations
-
 // XXX Remove once https://github.com/tschaub/es-main/pull/10 merged
 declare module 'es-main' {
   export default function (meta: ImportMeta): boolean;
---
+++ types/index.d.ts
@@ -0,0 +1,6 @@
+/* This file is a part of @mdn/browser-compat-data
+ * See LICENSE file for more information. */
+
+export type InternalSupportStatement = SupportStatement | 'mirror';
+
+export * from '../types';
