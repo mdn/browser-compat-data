0629c1d652c017bba78e06efa7579fc030112e22
Queen Vinyl Da.i'gyu-Kazotetsu <vinyldarkscratch@gmail.com>
2022-06-14 15:23:19 -0700

    Remove unused types in mirroring migration script

--- scripts/migrations/009-mirror.ts
+++ scripts/migrations/009-mirror.ts
@@ -1,13 +1,7 @@
 /* This file is a part of @mdn/browser-compat-data
  * See LICENSE file for more information. */
 
-import {
-  CompatData,
-  BrowserName,
-  Identifier,
-  ReleaseStatement,
-  SimpleSupportStatement,
-} from '../../types/types.js';
+import { CompatData, BrowserName } from '../../types/types.js';
 import {
   InternalSupportStatement,
   InternalSupportBlock,
