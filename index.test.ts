86948eb78b870e7093a6863579fa3c809a4a5f01
Queen Vinyl Da.i'gyu-Kazotetsu <vinyldarkscratch@gmail.com>
2022-06-15 11:45:45 -0700

    Fix Identifier type

---
+++ index.test.ts
@@ -0,0 +1,18 @@
+/* This file is a part of @mdn/browser-compat-data
+ * See LICENSE file for more information. */
+
+import { CompatStatement } from './types/types.js';
+
+import assert from 'node:assert/strict';
+
+import bcd from './index.js';
+
+describe('Using BCD', () => {
+  it('subscript notation', () => {
+    const data: CompatStatement = bcd['api']['AbortController']['__compat'];
+  });
+
+  it('dot notation', () => {
+    const data: CompatStatement = bcd.api.AbortController.__compat;
+  });
+});
