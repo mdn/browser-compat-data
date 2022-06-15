f14e0b1e6bb2a39c8976cb7fa06ef9b97a3bd1af
Queen Vinyl Da.i'gyu-Kazotetsu <vinyldarkscratch@gmail.com>
2022-06-15 12:10:21 -0700

    Update unittest

--- index.test.ts
+++ index.test.ts
@@ -3,14 +3,19 @@
 
 import { CompatStatement } from './types/types.js';
 
+import assert from 'node:assert/strict';
+
 import bcd from './index.js';
 
 describe('Using BCD', () => {
   it('subscript notation', () => {
-    const data: CompatStatement = bcd['api']['AbortController']['__compat'];
+    const data: CompatStatement | undefined =
+      bcd['api']['AbortController']['__compat'];
+    assert.ok(data);
   });
 
   it('dot notation', () => {
-    const data: CompatStatement = bcd.api.AbortController.__compat;
+    const data: CompatStatement | undefined = bcd.api.AbortController.__compat;
+    assert.ok(data);
   });
 });
