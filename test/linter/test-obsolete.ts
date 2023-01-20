eaf9270da0cadcd70af31d5938d32e37762b0f47
Michaël Vanderheyden <hello@th3s4mur41.me>
2023-01-20 15:09:47 +0000

    fix: remove experimental flag and obsolete check

--- test/linter/test-obsolete.ts
+++ test/linter/test-obsolete.ts
@@ -129,7 +129,6 @@ export default {
     processData(logger, data);
   },
   exceptions: [
-    'css.types.length.rlh',
     'http.headers.Cache-Control.stale-if-error',
     'http.headers.Feature-Policy.layout-animations',
     'http.headers.Feature-Policy.legacy-image-formats',
