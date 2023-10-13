d18479dccb5b248535644533ef108f31e6263bea
Jean-Yves Perrier <jypenator@gmail.com>
2023-10-13 17:01:32 +0200

    Add support for arguments --chrome --firefox --all --desktop --mobile

--- scripts/update-browser-releases/index.ts
+++ scripts/update-browser-releases/index.ts
@@ -3,6 +3,31 @@
 
 import { updateChromiumFile } from './chrome-version.js';
 
+// Arguments:
+//  Engine selections:
+//   --chrome : update chrome
+//   --firefox : update firefox
+//   --all : update all
+//   If none of the three, equivalent of -all. If several, they add
+//  Mobile/Desktop selection
+//   --mobile: update all mobile
+//   --desktop: update all desktop
+//  If several they add; if none: equivalent to --mobile --desktop
+const updateAll =
+  !(
+    process.argv.indexOf('--all') > -1 ||
+    process.argv.indexOf('--chrome') > -1 ||
+    process.argv.indexOf('--firefox') > -1
+  ) || process.argv.indexOf('--all') > -1;
+const updateChrome = process.argv.indexOf('--chrome') > -1 || updateAll;
+//const updateFirefox = (process.argv.indexOf('--chrome') > -1) || updateAll;
+const updateAllTypes = !(
+  process.argv.indexOf('--mobile') > -1 ||
+  process.argv.indexOf('--desktop') > -1
+);
+const updateMobile = process.argv.indexOf('--mobile') > -1 || updateAllTypes;
+const updateDesktop = process.argv.indexOf('--desktop') > -1 || updateAllTypes;
+
 const options = {
   desktop: {
     bcdFile: './browsers/chrome.json',
@@ -28,8 +53,12 @@ const options = {
   },
 };
 
-console.log('Check Android for Desktop.');
-await updateChromiumFile(options.desktop);
+if (updateChrome && updateDesktop) {
+  console.log('Check Chrome for Desktop.');
+  await updateChromiumFile(options.desktop);
+}
 
-console.log('Check Android for Android.');
-await updateChromiumFile(options.android);
+if (updateChrome && updateMobile) {
+  console.log('Check Chrome for Android.');
+  await updateChromiumFile(options.android);
+}
