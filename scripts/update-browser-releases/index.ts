03930dcfde59754f16a2b54d227996834c3f7fe7
Jean-Yves Perrier <jypenator@gmail.com>
2023-10-18 16:07:21 +0200

    Add script to update {chrome|chrome_android|webview_android}.json with new browser versions (#20901)
    * Add script to update Chrome.json with new version
    
    * Remove extraneous log
    
    * Add missing jsdoc comment
    
    * Automatically prettify json output
    
    * Use dev for nightly
    
    * Use @actions/core for logs and errors
    
    * Revert to console – it was unnecessary
    
    * Prettier run
    
    * Support chrome for android too
    
    * Removed logs + prettier
    
    * Deal with missing entries
    
    * Bump @typescript-eslint/eslint-plugin from 6.7.4 to 6.7.5 (#20937)
    
    * Bump @typescript-eslint/parser from 6.7.4 to 6.7.5 (#20938)
    
    * Update dependencies (#20943)
    
    * Run the script
    
    * Add review-related changes
    
    * Add support for arguments --chrome --firefox --all --desktop --mobile
    
    * Add --help
    
    * Run prettier
    
    * Create utils.ts for functions common to all browsers
    
    * Add support for webview_android
    
    * Skip non-existent webview version
    
    * Run prettier
    
    * Update package.json
    
    Co-authored-by: Florian Scholz <fs@florianscholz.com>
    
    * Update scripts/update-browser-releases/index.ts
    
    Co-authored-by: Florian Scholz <fs@florianscholz.com>
    
    * Update scripts/update-browser-releases/chrome-version.ts
    
    Co-authored-by: Florian Scholz <fs@florianscholz.com>
    
    * Update scripts/update-browser-releases/chrome-version.ts
    
    Co-authored-by: Florian Scholz <fs@florianscholz.com>
    
    * Update scripts/update-browser-releases/utils.ts
    
    Co-authored-by: Florian Scholz <fs@florianscholz.com>
    
    * Actually uses updateChromiumReleases
    
    * Actually uses updateChromiumReleases (2)
    
    * Run prettier
    
    * Rename chrome-version.ts to chrome.ts
    
    * succesfully -> successfully
    
    * verison -> version
    
    * Add support for chalk-template
    
    * Adding a loop to shorten the code (part 1)
    
    * dev -> canary
    
    * Adding a loop to shorten the code (part 2)
    
    * Remove debug log
    
    * Run prettier
    
    * Add support for yargs
    
    * Add missing comment
    
    * Bump @typescript-eslint/eslint-plugin from 6.7.4 to 6.7.5 (#20937)
    
    * Bump @typescript-eslint/parser from 6.7.4 to 6.7.5 (#20938)
    
    * Update dependencies (#20943)
    
    * Reset package-lock.json
    
    * Reset chrome.json
    
    ---------
    
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>
    Co-authored-by: Queen Vinyl Da.i'gyu-Kazotetsu <vinyldarkscratch@gmail.com>
    Co-authored-by: Florian Scholz <fs@florianscholz.com>

---
+++ scripts/update-browser-releases/index.ts
@@ -0,0 +1,103 @@
+/* This file is a part of @mdn/browser-compat-data
+ * See LICENSE file for more information. */
+import yargs from 'yargs';
+
+import { updateChromiumReleases } from './chrome.js';
+
+const argv = yargs(process.argv.slice(2))
+  .usage('Usage: npm run update-browser-releases -- (flags)')
+  .option('chrome', {
+    describe: 'Update Google Chrome',
+    type: 'boolean',
+    group: 'Engine selection:',
+  })
+  .option('webview', {
+    describe: 'Update Google Webview',
+    type: 'boolean',
+    group: 'Engine selection:',
+  })
+  .option('all', {
+    describe: 'Update all browsers (default)',
+    type: 'boolean',
+    group: 'Engine selection:',
+  })
+  .option('desktop', {
+    describe: 'Update desktop releases',
+    type: 'boolean',
+    group: 'Device selection:',
+  })
+  .option('mobile', {
+    describe: 'Update mobile releases',
+    type: 'boolean',
+    group: 'Device selection:',
+  })
+  .option('alldevices', {
+    describe: 'Update all devices (default)',
+    type: 'boolean',
+    group: 'Device selection:',
+  })
+  .help()
+  .parse();
+
+// Read arguments
+const updateAllBrowsers = argv['all'] || !(argv['chrome'] || argv['webview']);
+const updateChrome = argv['chrome'] || updateAllBrowsers;
+const updateWebview = argv['webview'] || updateAllBrowsers;
+const updateAllDevices =
+  argv['alldevices'] || !(argv['mobile'] || argv['desktop']);
+const updateMobile = argv['mobile'] || updateAllDevices;
+const updateDesktop = argv['desktop'] || updateAllDevices;
+
+const options = {
+  desktop: {
+    bcdFile: './browsers/chrome.json',
+    bcdBrowserName: 'chrome',
+    browserEngine: 'Blink',
+    releaseBranch: 'stable',
+    betaBranch: 'beta',
+    nightlyBranch: 'canary',
+    releaseNoteCore: 'stable-channel-update-for-desktop',
+    firstRelease: 1,
+    skippedReleases: [82], // 82 was skipped during COVID
+    chromestatusURL: 'https://chromestatus.com/api/v0/channels',
+  },
+  android: {
+    bcdFile: './browsers/chrome_android.json',
+    bcdBrowserName: 'chrome_android',
+    browserEngine: 'Blink',
+    releaseBranch: 'stable',
+    betaBranch: 'beta',
+    nightlyBranch: 'canary',
+    releaseNoteCore: 'chrome-for-android-update',
+    firstRelease: 25,
+    skippedReleases: [82], // 82 was skipped during COVID
+    chromestatusURL: 'https://chromestatus.com/api/v0/channels',
+  },
+  webview_android: {
+    bcdFile: './browsers/webview_android.json',
+    bcdBrowserName: 'webview_android',
+    browserEngine: 'Blink',
+    releaseBranch: 'stable',
+    betaBranch: 'beta',
+    nightlyBranch: 'canary',
+    releaseNoteCore: 'chrome-for-android-update',
+    firstRelease: 37,
+    skippedReleases: [82], // 82 was skipped during COVID
+    chromestatusURL: 'https://chromestatus.com/api/v0/channels',
+  },
+};
+
+if (updateChrome && updateDesktop) {
+  console.log('Check Chrome for Desktop.');
+  await updateChromiumReleases(options.desktop);
+}
+
+if (updateChrome && updateMobile) {
+  console.log('Check Chrome for Android.');
+  await updateChromiumReleases(options.android);
+}
+
+if (updateWebview && updateMobile) {
+  console.log('Check Webview for Android.');
+  await updateChromiumReleases(options.webview_android);
+}
