d2d427dfa2472cf935c156343da43580b967721c
Isabel Schöps Thiel @github.com <si_foundation@icloud.com>
2023-11-26 02:10:41 +0100

    Delete utils directory

--- utils/index.ts
+++
@@ -1,9 +0,0 @@
-/* This file is a part of @mdn/browser-compat-data
- * See LICENSE file for more information. */
-
-import iterSupport from './iter-support.js';
-import query from './query.js';
-import walk from './walk.js';
-import normalizePath from './normalize-path.js';
-
-export { iterSupport, normalizePath, query, walk };
