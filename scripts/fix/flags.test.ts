5a50e8fb72d36e69ee192662657c956d30f6711e
Queen Vinyl Da.i'gyu-Kazotetsu <vinyldarkscratch@gmail.com>
2023-12-08 03:05:00 -0800

    Fix unittest

--- scripts/fix/flags.test.ts
+++ scripts/fix/flags.test.ts
@@ -3,490 +3,252 @@
 
 import assert from 'node:assert/strict';
 
+import { SupportStatement } from '../../types/types.js';
+
 import { removeIrrelevantFlags } from './flags.js';
 
-const tests = [
+const tests: { input: SupportStatement; output: SupportStatement }[] = [
   {
-    input: {
-      test1: {
-        __compat: {
-          support: {
-            chrome: [
-              {
-                version_added: '70',
-              },
-              {
-                version_added: '21',
-                version_removed: '65',
-                flags: [
-                  {
-                    type: 'preference',
-                    name: '#service-worker-payment-apps',
-                    value_to_set: 'Enabled',
-                  },
-                ],
-              },
-            ],
-          },
-          status: {
-            experimental: true,
-            standard_track: false,
-            deprecated: false,
-          },
-        },
+    input: [
+      {
+        version_added: '70',
       },
-    },
+      {
+        version_added: '21',
+        version_removed: '65',
+        flags: [
+          {
+            type: 'preference',
+            name: '#service-worker-payment-apps',
+            value_to_set: 'Enabled',
+          },
+        ],
+      },
+    ],
     output: {
-      test1: {
-        __compat: {
-          support: {
-            chrome: {
-              version_added: '70',
-            },
-          },
-          status: {
-            experimental: true,
-            standard_track: false,
-            deprecated: false,
-          },
-        },
+      version_added: '70',
+    },
+  },
+  {
+    input: [
+      {
+        version_added: '62',
       },
+      {
+        version_added: '21',
+        version_removed: '80',
+        flags: [
+          {
+            type: 'preference',
+            name: '#service-worker-payment-apps',
+            value_to_set: 'Enabled',
+          },
+        ],
+      },
+    ],
+    output: {
+      version_added: '62',
+    },
+  },
+  {
+    input: [
+      {
+        version_added: '62',
+      },
+      {
+        version_added: '21',
+        flags: [
+          {
+            type: 'preference',
+            name: '#service-worker-payment-apps',
+            value_to_set: 'Enabled',
+          },
+        ],
+      },
+    ],
+    output: {
+      version_added: '62',
+    },
+  },
+  {
+    input: [
+      {
+        version_added: '42',
+        flags: [
+          {
+            type: 'preference',
+            name: '#enable-experimental-web-features',
+            value_to_set: 'Enabled',
+          },
+        ],
+      },
+      {
+        version_added: '21',
+        version_removed: '45',
+        flags: [
+          {
+            type: 'preference',
+            name: '#service-worker-payment-apps',
+            value_to_set: 'Enabled',
+          },
+        ],
+      },
+    ],
+    output: {
+      version_added: '42',
+      flags: [
+        {
+          type: 'preference',
+          name: '#enable-experimental-web-features',
+          value_to_set: 'Enabled',
+        },
+      ],
     },
   },
   {
     input: {
-      test2: {
-        __compat: {
-          support: {
-            chrome: [
-              {
-                version_added: '62',
-              },
-              {
-                version_added: '21',
-                version_removed: '80',
-                flags: [
-                  {
-                    type: 'preference',
-                    name: '#service-worker-payment-apps',
-                    value_to_set: 'Enabled',
-                  },
-                ],
-              },
-            ],
-          },
-          status: {
-            experimental: true,
-            standard_track: false,
-            deprecated: false,
-          },
+      version_added: '42',
+      version_removed: '43',
+      flags: [
+        {
+          type: 'preference',
+          name: '#enable-experimental-web-features',
+          value_to_set: 'Enabled',
         },
-      },
+      ],
     },
     output: {
-      test2: {
-        __compat: {
-          support: {
-            chrome: {
-              version_added: '62',
-            },
-          },
-          status: {
-            experimental: true,
-            standard_track: false,
-            deprecated: false,
-          },
-        },
-      },
+      version_added: false,
     },
   },
   {
-    input: {
-      test3: {
-        __compat: {
-          support: {
-            chrome: [
-              {
-                version_added: '62',
-              },
-              {
-                version_added: '21',
-                flags: [
-                  {
-                    type: 'preference',
-                    name: '#service-worker-payment-apps',
-                    value_to_set: 'Enabled',
-                  },
-                ],
-              },
-            ],
-          },
-          status: {
-            experimental: true,
-            standard_track: false,
-            deprecated: false,
-          },
-        },
+    input: [
+      {
+        version_added: '80',
       },
-    },
+      {
+        version_added: '21',
+        version_removed: '80',
+        flags: [
+          {
+            type: 'preference',
+            name: '#service-worker-payment-apps',
+            value_to_set: 'Enabled',
+          },
+        ],
+      },
+    ],
     output: {
-      test3: {
-        __compat: {
-          support: {
-            chrome: {
-              version_added: '62',
-            },
-          },
-          status: {
-            experimental: true,
-            standard_track: false,
-            deprecated: false,
-          },
-        },
-      },
+      version_added: '80',
     },
   },
   {
-    input: {
-      test4: {
-        __compat: {
-          support: {
-            chrome: [
-              {
-                version_added: '42',
-                flags: [
-                  {
-                    type: 'preference',
-                    name: '#enable-experimental-web-features',
-                    value_to_set: 'Enabled',
-                  },
-                ],
-              },
-              {
-                version_added: '21',
-                version_removed: '45',
-                flags: [
-                  {
-                    type: 'preference',
-                    name: '#service-worker-payment-apps',
-                    value_to_set: 'Enabled',
-                  },
-                ],
-              },
-            ],
+    input: [
+      {
+        version_added: '79',
+        flags: [
+          {
+            type: 'preference',
+            name: 'enable-experimental-web-platform-features',
+            value_to_set: 'enabled',
           },
-          status: {
-            experimental: true,
-            standard_track: false,
-            deprecated: false,
-          },
-        },
+        ],
       },
-    },
+      {
+        version_added: '12',
+        version_removed: '79',
+      },
+    ],
+    output: [
+      {
+        version_added: '79',
+        flags: [
+          {
+            type: 'preference',
+            name: 'enable-experimental-web-platform-features',
+            value_to_set: 'enabled',
+          },
+        ],
+      },
+      {
+        version_added: '12',
+        version_removed: '79',
+      },
+    ],
+  },
+  {
+    input: [
+      {
+        version_added: '86',
+      },
+      {
+        version_added: '21',
+        flags: [
+          {
+            type: 'preference',
+            name: '#service-worker-payment-apps',
+            value_to_set: 'Enabled',
+          },
+        ],
+      },
+    ],
     output: {
-      test4: {
-        __compat: {
-          support: {
-            chrome: {
-              version_added: '42',
-              flags: [
-                {
-                  type: 'preference',
-                  name: '#enable-experimental-web-features',
-                  value_to_set: 'Enabled',
-                },
-              ],
-            },
-          },
-          status: {
-            experimental: true,
-            standard_track: false,
-            deprecated: false,
-          },
-        },
-      },
+      version_added: '86',
     },
   },
   {
-    input: {
-      test5: {
-        __compat: {
-          support: {
-            chrome: {
-              version_added: '42',
-              version_removed: '43',
-              flags: [
-                {
-                  type: 'preference',
-                  name: '#enable-experimental-web-features',
-                  value_to_set: 'Enabled',
-                },
-              ],
-            },
+    input: [
+      {
+        version_added: '79',
+        version_removed: '80',
+        flags: [
+          {
+            type: 'preference',
+            name: 'WebVR',
           },
-          status: {
-            experimental: true,
-            standard_track: false,
-            deprecated: false,
-          },
-        },
+        ],
       },
-    },
+      {
+        version_added: '15',
+        version_removed: '79',
+      },
+    ],
     output: {
-      test5: {
-        __compat: {
-          support: {
-            chrome: {
-              version_added: false,
-            },
-          },
-          status: {
-            experimental: true,
-            standard_track: false,
-            deprecated: false,
-          },
-        },
-      },
+      version_added: '15',
+      version_removed: '79',
     },
   },
   {
-    input: {
-      test6: {
-        __compat: {
-          support: {
-            chrome: [
-              {
-                version_added: '80',
-              },
-              {
-                version_added: '21',
-                version_removed: '80',
-                flags: [
-                  {
-                    type: 'preference',
-                    name: '#service-worker-payment-apps',
-                    value_to_set: 'Enabled',
-                  },
-                ],
-              },
-            ],
-          },
-          status: {
-            experimental: true,
-            standard_track: false,
-            deprecated: false,
-          },
-        },
+    input: [
+      {
+        version_added: 'preview',
       },
-    },
-    output: {
-      test6: {
-        __compat: {
-          support: {
-            chrome: {
-              version_added: '80',
-            },
+      {
+        version_added: '105',
+        flags: [
+          {
+            type: 'preference',
+            name: 'layout.css.font-tech.enabled',
+            value_to_set: 'true',
           },
-          status: {
-            experimental: true,
-            standard_track: false,
-            deprecated: false,
-          },
-        },
+        ],
       },
-    },
-  },
-  {
-    input: {
-      test7: {
-        __compat: {
-          support: {
-            edge: [
-              {
-                version_added: '79',
-                flags: [
-                  {
-                    type: 'preference',
-                    name: 'enable-experimental-web-platform-features',
-                    value_to_set: 'enabled',
-                  },
-                ],
-              },
-              {
-                version_added: '12',
-                version_removed: '79',
-              },
-            ],
-          },
-          status: {
-            experimental: true,
-            standard_track: false,
-            deprecated: false,
-          },
-        },
+    ],
+    output: [
+      {
+        version_added: 'preview',
       },
-    },
-    output: {
-      test7: {
-        __compat: {
-          support: {
-            edge: [
-              {
-                version_added: '79',
-                flags: [
-                  {
-                    type: 'preference',
-                    name: 'enable-experimental-web-platform-features',
-                    value_to_set: 'enabled',
-                  },
-                ],
-              },
-              {
-                version_added: '12',
-                version_removed: '79',
-              },
-            ],
+      {
+        version_added: '105',
+        flags: [
+          {
+            type: 'preference',
+            name: 'layout.css.font-tech.enabled',
+            value_to_set: 'true',
           },
-          status: {
-            experimental: true,
-            standard_track: false,
-            deprecated: false,
-          },
-        },
+        ],
       },
-    },
-  },
-  {
-    input: {
-      test8: {
-        __compat: {
-          support: {
-            chrome: [
-              {
-                version_added: '86',
-              },
-              {
-                version_added: '21',
-                flags: [
-                  {
-                    type: 'preference',
-                    name: '#service-worker-payment-apps',
-                    value_to_set: 'Enabled',
-                  },
-                ],
-              },
-            ],
-          },
-          status: {
-            experimental: true,
-            standard_track: false,
-            deprecated: false,
-          },
-        },
-      },
-    },
-    output: {
-      test8: {
-        __compat: {
-          support: {
-            chrome: {
-              version_added: '86',
-            },
-          },
-          status: {
-            experimental: true,
-            standard_track: false,
-            deprecated: false,
-          },
-        },
-      },
-    },
-  },
-  {
-    input: {
-      test9: {
-        __compat: {
-          support: {
-            edge: [
-              {
-                version_added: '79',
-                version_removed: '80',
-                flags: [
-                  {
-                    type: 'preference',
-                    name: 'WebVR',
-                  },
-                ],
-              },
-              {
-                version_added: '15',
-                version_removed: '79',
-              },
-            ],
-          },
-        },
-      },
-    },
-    output: {
-      test9: {
-        __compat: {
-          support: {
-            edge: {
-              version_added: '15',
-              version_removed: '79',
-            },
-          },
-        },
-      },
-    },
-  },
-  {
-    input: {
-      test10: {
-        __compat: {
-          support: {
-            firefox: [
-              {
-                version_added: 'preview',
-              },
-              {
-                version_added: '105',
-                flags: [
-                  {
-                    type: 'preference',
-                    name: 'layout.css.font-tech.enabled',
-                    value_to_set: 'true',
-                  },
-                ],
-              },
-            ],
-          },
-        },
-      },
-    },
-    output: {
-      test10: {
-        __compat: {
-          support: {
-            firefox: [
-              {
-                version_added: 'preview',
-              },
-              {
-                version_added: '105',
-                flags: [
-                  {
-                    type: 'preference',
-                    name: 'layout.css.font-tech.enabled',
-                    value_to_set: 'true',
-                  },
-                ],
-              },
-            ],
-          },
-        },
-      },
-    },
+    ],
   },
 ];
 
@@ -494,14 +256,9 @@ describe('fix -> flags', () => {
   let i = 1;
   for (const test of tests) {
     it(`Test #${i}`, () => {
-      const expected = JSON.stringify(test['output'], null, 2);
-      const output = JSON.stringify(
-        JSON.parse(JSON.stringify(test['input']), removeIrrelevantFlags),
-        null,
-        2,
-      );
+      const result = removeIrrelevantFlags(test.input);
 
-      assert.deepStrictEqual(expected, output);
+      assert.deepStrictEqual(result, test.output);
     });
 
     i += 1;
