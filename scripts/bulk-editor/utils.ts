67697b74858f73617f1c3f2a3ba21746f670d946
Queen Vinyl Da.i'gyu-Kazotetsu <vinyldarkscratch@gmail.com>
2024-04-05 04:55:10 -0700

    Walk file structure instead of attempting to guess it

--- scripts/bulk-editor/utils.ts
+++ scripts/bulk-editor/utils.ts
@@ -1,47 +1,17 @@
 /* This file is a part of @mdn/browser-compat-data
  * See LICENSE file for more information. */
 
-import fsp from 'node:fs/promises';
 import path from 'node:path';
+import { fileURLToPath } from 'node:url';
 import fs from 'node:fs';
 
 import chalk from 'chalk-template';
+import { fdir } from 'fdir';
 
-/**
- * bcdAssociatedFilename - Generate the filename containing the bcd ID
- * @param bcdID The bcd ID we are looking for
- * @param path The path to the JSON files
- * @returns A string with the filename
- */
-const bcdAssociatedFilename = (bcdID: string, path?: string): string => {
-  const [bcdDomain, top, second, third] = bcdID.split('.');
+import { dataFolders } from '../../index.js';
+import walk from '../../utils/walk.js';
 
-  if (!bcdDomain || !top || !second) {
-    console.log(chalk`{red Not a terminal bcd-id.}`);
-    return '';
-  }
-
-  // Special case for html.elements.input.type_
-  let fixedThird = third;
-  if (
-    bcdDomain === 'html' &&
-    top === 'elements' &&
-    second === 'input' &&
-    third.startsWith('type_')
-  ) {
-    fixedThird = third.replace('type_', '');
-  }
-
-  // Normal case: Test if the second file does exists
-  let filename = `${path ? path : ''}${bcdDomain}/${top}/${second}.json`;
-  try {
-    fsp.stat(filename);
-  } catch (e) {
-    // This is the special case for JS namespace (Intl, Temporal, …)
-    filename = `${path ? path : ''}${bcdDomain}/${top}/${second}/${fixedThird}.json`;
-  }
-  return filename;
-};
+const dirname = fileURLToPath(new URL('.', import.meta.url));
 
 const performUpdate = (key, json, updater) => {
   const parts = key.split('.');
@@ -49,35 +19,47 @@ const performUpdate = (key, json, updater) => {
     console.warn('Key not found in file!');
     return json;
   }
-  json[parts[0]] =
-    parts.length === 1
-      ? updater(json[parts[0]])
-      : performUpdate(parts.slice(1).join('.'), json[parts[0]], updater);
+  if (parts.length === 1) {
+    json[parts[0]]['__compat'] = updater(json[parts[0]]['__compat']);
+  } else {
+    json[parts[0]] = performUpdate(
+      parts.slice(1).join('.'),
+      json[parts[0]],
+      updater,
+    );
+  }
   return json;
 };
 
 export const updateFeatures = (featureIDs, updater) => {
-  for (const featureID of featureIDs) {
-    // XXX Walk the file tree instead
-    const filename = bcdAssociatedFilename(featureID);
+  for (const dir of dataFolders) {
+    const paths = new fdir()
+      .withBasePath()
+      .filter((fp) => fp.endsWith('.json'))
+      .crawl(path.join(dirname, '..', '..', dir))
+      .sync() as string[];
 
-    if (
-      fs.existsSync(filename) &&
-      path.extname(filename) === '.json' &&
-      !filename.endsWith('.schema.json')
-    ) {
-      console.log(chalk`{yellow Updating ${featureID}...}`);
-      const json = JSON.parse(fs.readFileSync(filename, 'utf-8'));
-      const newJson = performUpdate(featureID, json, updater);
-      fs.writeFileSync(
-        filename,
-        JSON.stringify(newJson, undefined, 2) + '\n',
-        'utf-8',
-      );
-    } else {
-      console.warn(
-        chalk`{yellow Warning! Could not find file for ${featureID}!}`,
-      );
+    for (const fp of paths) {
+      const rawcontents = fs.readFileSync(fp);
+      const contents = JSON.parse(rawcontents.toString('utf8'));
+      let changed = false;
+
+      const walker = walk(undefined, contents);
+      for (const { path: featureID } of walker) {
+        if (featureIDs.includes(featureID)) {
+          console.log(chalk`{yellow Updating ${featureID}...}`);
+          performUpdate(featureID, contents, updater);
+          changed = true;
+        }
+      }
+
+      if (changed) {
+        fs.writeFileSync(
+          fp,
+          JSON.stringify(contents, undefined, 2) + '\n',
+          'utf-8',
+        );
+      }
     }
   }
 };
