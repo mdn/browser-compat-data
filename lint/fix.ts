f8d944a9507952918517dd4ca745d5803920849e
Claas Augner <caugner@mozilla.com>
2024-10-08 19:15:33 +0200

    enhance(lint/fix): add --only option

--- lint/fix.ts
+++ lint/fix.ts
@@ -21,14 +21,36 @@ import fixLinks from './fixer/links.js';
 import fixMDNURLs from './fixer/mdn-urls.js';
 import fixStatus from './fixer/status.js';
 import fixMirror from './fixer/mirror.js';
+import { LintOptions } from './utils.js';
 
 const dirname = fileURLToPath(new URL('.', import.meta.url));
 
+const FIXES = Object.freeze({
+  browser_order: fixBrowserOrder,
+  feature_order: fixFeatureOrder,
+  property_order: fixPropertyOrder,
+  statement_order: fixStatementOrder,
+  descriptions: fixDescriptions,
+  flags: fixFlags,
+  links: fixLinks,
+  mdn_urls: fixMDNURLs,
+  status: fixStatus,
+  mirror: fixMirror,
+});
+
 /**
  * Recursively load one or more files and/or directories passed as arguments and perform automatic fixes.
+ * @param options The lint options
  * @param files The files to load and perform fix upon
  */
-const load = async (...files: string[]): Promise<void> => {
+const load = async (
+  options: LintOptions,
+  ...files: string[]
+): Promise<void> => {
+  const fixes = Object.entries(FIXES)
+    .filter(([key]) => !options.only || options.only.includes(key))
+    .map(([, fix]) => fix);
+
   for (let file of files) {
     if (file.indexOf(dirname) !== 0) {
       file = path.resolve(dirname, '..', file);
@@ -45,25 +67,14 @@ const load = async (...files: string[]): Promise<void> => {
 
     if (fsStats.isFile()) {
       if (path.extname(file) === '.json' && !file.endsWith('.schema.json')) {
-        if (!file.includes('/browsers/')) {
-          fixBrowserOrder(file);
-          fixFeatureOrder(file);
-          fixStatementOrder(file);
-          fixDescriptions(file);
-          fixFlags(file);
-          fixLinks(file);
-          fixMDNURLs(file);
-          fixStatus(file);
-          fixMirror(file);
-        }
-        fixPropertyOrder(file);
+        fixes.forEach((fix) => fix(file));
       }
     } else {
       const subFiles = (await fs.readdir(file)).map((subfile) =>
         path.join(file, subfile),
       );
 
-      await load(...subFiles);
+      await load(options, ...subFiles);
     }
   }
 };
@@ -71,21 +82,26 @@ const load = async (...files: string[]): Promise<void> => {
 /**
  * Fix any errors in specified file(s) and/or folder(s), or all of BCD
  * @param files The file(s) and/or folder(s) to fix. Leave undefined for everything.
+ * @param options Lint options
+ * @returns Promise<void>
  */
-const main = async (files: string[]) => {
-  load(...files);
+const main = async (files: string[], options: LintOptions) => {
+  load(options, ...files);
 };
 
 if (esMain(import.meta)) {
-  const { argv } = yargs(hideBin(process.argv)).command(
-    '$0 [files..]',
-    false,
-    (yargs) =>
+  const { argv } = yargs(hideBin(process.argv))
+    .command('$0 [files..]', false, (yargs) =>
       yargs.positional('files...', {
         description: 'The files to fix (leave blank to test everything)',
         type: 'string',
       }),
-  );
+    )
+    .option('only', {
+      array: true,
+      description: 'The checks to run',
+    })
+    .choices('only', Object.keys(FIXES));
 
   const {
     files = [
@@ -101,9 +117,10 @@ if (esMain(import.meta)) {
       'webdriver',
       'webextensions',
     ],
-  } = argv as any;
+    only,
+  } = argv as { files?: string[] } & LintOptions;
 
-  await main(files);
+  await main(files, { only });
 }
 
 export default load;
