a3fe957213719314a6ddab95775b364179fa83c9
Claas Augner <caugner@mozilla.com>
2024-10-19 00:01:27 +0200

    refactor(lint): make it async

--- lint/linter/test-links.ts
+++ lint/linter/test-links.ts
@@ -27,22 +27,22 @@ interface LinkError {
  * @param regexp The regex to test with
  * @param matchHandler The callback
  */
-const processLink = (
+const processLink = async (
   errors: LinkError[],
   actual: string,
   regexp: string | RegExp,
-  matchHandler: (match: RegExpMatchArray) => {
+  matchHandler: (match: RegExpMatchArray) => Promise<{
     issue: string;
     expected?: string;
     actualLink?: string;
-  } | null,
-): void => {
+  } | null>,
+): Promise<void> => {
   const re = new RegExp(regexp, 'g');
   let match;
   while ((match = re.exec(actual)) !== null) {
     const pos = indexToPosRaw(actual, match.index);
     const posString = indexToPos(actual, match.index);
-    const result = matchHandler(match);
+    const result = await matchHandler(match);
 
     if (result) {
       const { issue, expected, actualLink = match[0] } = result;
@@ -62,7 +62,7 @@ const processLink = (
  * @param rawData The raw contents of the file to test
  * @returns A list of errors found in the links
  */
-export const processData = (rawData: string): LinkError[] => {
+export const processData = async (rawData: string): Promise<LinkError[]> => {
   const errors: LinkError[] = [];
 
   let actual = rawData;
@@ -74,78 +74,78 @@ export const processData = (rawData: string): LinkError[] => {
   }
   /* c8 ignore stop */
 
-  processLink(
+  await processLink(
     // use https://bugzil.la/1000000 instead
     errors,
     actual,
     /https?:\/\/bugzilla\.mozilla\.org\/show_bug\.cgi\?id=(\d+)/g,
-    (match) => ({
+    async (match) => ({
       issue: 'Use shortenable URL',
       expected: `https://bugzil.la/${match[1]}`,
     }),
   );
 
-  processLink(
+  await processLink(
     // use https://crbug.com/100000 instead
     errors,
     actual,
     /https?:\/\/(issues\.chromium\.org)\/issues\/(\d+)/g,
-    (match) => ({
+    async (match) => ({
       issue: 'Use shortenable URL',
       expected: `https://crbug.com/${match[2]}`,
     }),
   );
 
-  processLink(
+  await processLink(
     // use https://crbug.com/100000 instead
     errors,
     actual,
     /https?:\/\/(bugs\.chromium\.org|code\.google\.com)\/p\/chromium\/issues\/detail\?id=(\d+)/g,
-    (match) => ({
+    async (match) => ({
       issue: 'Use shortenable URL',
       expected: `https://crbug.com/${match[2]}`,
     }),
   );
 
-  processLink(
+  await processLink(
     // use https://crbug.com/category/100000 instead
     errors,
     actual,
     /https?:\/\/(bugs\.chromium\.org|code\.google\.com)\/p\/((?!chromium)\w+)\/issues\/detail\?id=(\d+)/g,
-    (match) => ({
+    async (match) => ({
       issue: 'Use shortenable URL',
       expected: `https://crbug.com/${match[2]}/${match[3]}`,
     }),
   );
 
-  processLink(
+  await processLink(
     // use https://crbug.com/category/100000 instead
     errors,
     actual,
     /https?:\/\/chromium\.googlesource\.com\/chromium\/src\/\+\/([\w\d]+)/g,
-    (match) => ({
+    async (match) => ({
       issue: 'Use shortenable URL',
       expected: `https://crrev.com/${match[1]}`,
     }),
   );
 
-  processLink(
+  await processLink(
     // use https://webkit.org/b/100000 instead
     errors,
     actual,
     /https?:\/\/bugs\.webkit\.org\/show_bug\.cgi\?id=(\d+)/g,
-    (match) => ({
+    async (match) => ({
       issue: 'Use shortenable URL',
       expected: `https://webkit.org/b/${match[1]}`,
     }),
   );
 
-  processLink(
+  await processLink(
     // Bug links should use HTTPS and have "bug ###" as link text ("Bug ###" only at the beginning of notes/sentences).
     errors,
     actual,
     /(\w*\s?)<a href='((https?):\/\/(bugzil\.la|crbug\.com|webkit\.org\/b)\/(\d+))'>(.*?)<\/a>/g,
-    (match) => {
+    async (match) => {
       const [, before, url, protocol, domain, bugId, linkText] = match;
 
       if (protocol !== 'https') {
@@ -182,11 +182,11 @@ export const processData = (rawData: string): LinkError[] => {
     },
   );
 
-  processLink(
+  await processLink(
     errors,
     actual,
     /(https?):\/\/((?:[a-z][a-z0-9-]*\.)*)?developer.mozilla.org\/(.*?)(?=["'\s])/g,
-    (match) => {
+    async (match) => {
       const [, protocol, subdomain, path] = match;
 
       if (protocol !== 'https') {
@@ -222,21 +222,21 @@ export const processData = (rawData: string): LinkError[] => {
     },
   );
 
-  processLink(
+  await processLink(
     errors,
     actual,
     /https?:\/\/developer.microsoft.com\/(\w\w-\w\w)\/(.*?)(?=["'\s])/g,
-    (match) => ({
+    async (match) => ({
       issue: 'Use non-localized Microsoft Developer URL',
       expected: `https://developer.microsoft.com/${match[2]}`,
     }),
   );
 
-  processLink(
+  await processLink(
     errors,
     actual,
     /<a href='([^'>]+)'>((?:.(?<!<\/a>))*.)<\/a>/g,
-    (match) => {
+    async (match) => {
       if (new URL(match[1]).hostname === null) {
         return {
           issue: 'Include hostname in URL',
@@ -263,8 +263,8 @@ export default {
    * @param root The data to test
    * @param root.rawdata The raw contents of the file to test
    */
-  check: (logger: Logger, { rawdata }: LinterData) => {
-    const errors = processData(rawdata);
+  check: async (logger: Logger, { rawdata }: LinterData) => {
+    const errors = await processData(rawdata);
 
     for (const error of errors) {
       logger.error(
