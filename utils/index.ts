eb5ab031525d8ee59db8338291154d707a323ce4
Claas Augner <caugner@mozilla.com>
2025-05-23 15:56:13 +0200

    feat(utils): add spawnAsync() util

--- utils/index.ts
+++ utils/index.ts
@@ -5,7 +5,16 @@ import execAsync from './exec-async.js';
 import iterSupport from './iter-support.js';
 import query from './query.js';
 import spawn from './spawn.js';
+import spawnAsync from './spawn-async.js';
 import walk from './walk.js';
 import normalizePath from './normalize-path.js';
 
-export { execAsync, iterSupport, normalizePath, query, spawn, walk };
+export {
+  execAsync,
+  iterSupport,
+  normalizePath,
+  query,
+  spawn,
+  spawnAsync,
+  walk,
+};
