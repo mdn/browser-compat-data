05e52bf4e7226a31d5b5f78391d91347a1dcf259
Claas Augner <caugner@mozilla.com>
2025-08-27 11:16:09 +0200

    refactor(lint): extract createAjv() to utils

--- utils/index.ts
+++ utils/index.ts
@@ -1,6 +1,7 @@
 /* This file is a part of @mdn/browser-compat-data
  * See LICENSE file for more information. */
 
+import { createAjv } from './ajv.js';
 import iterSupport from './iter-support.js';
 import query from './query.js';
 import spawn from './spawn.js';
@@ -8,4 +9,12 @@ import spawnAsync from './spawn-async.js';
 import walk from './walk.js';
 import normalizePath from './normalize-path.js';
 
-export { iterSupport, normalizePath, query, spawn, spawnAsync, walk };
+export {
+  createAjv,
+  iterSupport,
+  normalizePath,
+  query,
+  spawn,
+  spawnAsync,
+  walk,
+};
