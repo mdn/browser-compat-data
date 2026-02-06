df3c2bfe0a4f3c2660f081dd559721c04b80b4cb
Claas Augner <495429+caugner@users.noreply.github.com>
2026-02-06 15:18:53 +0100

    refactor: migrate from TypeScript to JavaScript (#28651)
    * refactor: migrate from TypeScript to JavaScript
    
    * fix(diff-features): enumerate features with tsx if necessary
    
    * fixup! Merge branch 'main' into un-typescript
    
    * chore(eslint): forbid `@typedef`
    
    * refactor: migrate `@typedef` imports to `@import`
    
    * chore(eslint): enable jsdoc/prefer-import-tag
    
    * refactor: replace `import()` in JSDoc with `@import`
    
    * chore(eslint): refine build ignore
    
    It was also ignoring `scripts/build`.
    
    * chore: run `eslint --fix`
    
    * fixup! refactor: migrate `@typedef` imports to `@import`
    
    * fix(scripts/build): add missing async option
    
    Helps TypeScript to infer the right return type.
    
    * chore(tsconfig): remove unnecessary `include`
    
    * chore(tsconfig): remove `skipLibCheck`
    
    * fix(types): import from `build/types.d.ts`
    
    * fix: resolve newly surfaced type errors
    
    * chore(eslint): remove typescript config/plugins
    
    * chore(deps-dev): remove unused dependencies
    
    * fix(eslint): use latest ecmaVersion
    
    As recommended, see: https://eslint.org/docs/latest/use/configure/language-options#specifying-javascript-options
    
    * chore(tsconfig): include tests in type checking
    
    * chore(lint): remove duplicate type imports
    
    * chore(lint): avoid invalid test value
    
    * chore(lint): fix some types
    
    * fixup! chore(lint): avoid invalid test value
    
    * fixup! chore(lint): fix some types
    
    * chore(utils): add missing test for __compat
    
    * chore(eslint): ignore `import/no-unresolved` errors for `yargs/helpers`
    
    * chore(lint): remove unnessary type annotation
    
    * chore(lint): refine type
    
    * fix(yargs): avoid callback + use parseSync() for proper typing
    
    * chore(scripts/diff-features): specify tsx version
    
    Same as in `package.json` in main.
    
    * fixup! fix(yargs): avoid callback + use parseSync() for proper typing
    
    * fixup! fix(yargs): avoid callback + use parseSync() for proper typing
    
    * chore: use arrow function type syntax
    
    * refactor(lint): extract types to d.ts file
    
    * fix(lint/types): unmark path.{full,category} as optional
    
    * test(lint): fix incomplete paths in test cases
    
    * chore(lint): handle undefined rawdata
    
    * test(lint): add missing version_added field
    
    * chore(types): introduce InternalDataType
    
    * chore(eslint): bring back `@typescript/eslint`
    
    * chore(tsconfig): re-enable skipLibCheck
    
    Otherwise we stumble over web-features type
    import without file extension.
    
    * chore(lint): avoid cast to unknown
    
    * chore(eslint): forbid unknown type in JSDoc
    
    * chore(mocha): remove unused mjs extension
    
    * chore(eslint): use jsdoc/recommended-typescript-flavor
    
    * chore(eslint): fix type import
    
    * chore(eslint): ignore no-empty-function once
    
    * chore(eslint): disable duplicate no-unused-vars rule
    
    * chore(eslint): use single no-unused-var + refine
    
    * style(eslint): remove unused vars
    
    * style(eslint): resolve no-unused-vars issue

---
+++ index.js
@@ -0,0 +1,60 @@
+/* This file is a part of @mdn/browser-compat-data
+ * See LICENSE file for more information. */
+
+/** @import {CompatData} from './types/types.js' */
+
+import fs from 'node:fs/promises';
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+
+import { fdir } from 'fdir';
+
+import extend from './scripts/lib/extend.js';
+import dataFolders from './scripts/lib/data-folders.js';
+import { normalizePath, walk } from './utils/index.js';
+
+const dirname = fileURLToPath(new URL('.', import.meta.url));
+
+/**
+ * Recursively load one or more directories passed as arguments.
+ * @param {...string} dirs The directories to load
+ * @returns {Promise<CompatData>} All of the browser compatibility data
+ */
+const load = async (...dirs) => {
+  const result = {};
+
+  for (const dir of dirs) {
+    const paths = /** @type {string[]} */ (
+      new fdir()
+        .withBasePath()
+        .filter((fp) => fp.endsWith('.json'))
+        .crawl(path.join(dirname, dir))
+        .sync()
+    );
+
+    for (const fp of paths) {
+      try {
+        const rawcontents = await fs.readFile(fp);
+        /** @type {CompatData} */
+        const contents = JSON.parse(rawcontents.toString('utf8'));
+
+        // Add source_file props
+        const walker = walk(undefined, contents);
+        for (const { compat } of walker) {
+          compat.source_file = normalizePath(path.relative(dirname, fp));
+        }
+
+        extend(result, contents);
+        /* c8 ignore start */
+      } catch {
+        // Skip invalid JSON. Tests will flag the problem separately.
+        continue;
+      }
+      /* c8 ignore stop */
+    }
+  }
+
+  return /** @type {CompatData} */ (result);
+};
+
+export default await load(...dataFolders);
