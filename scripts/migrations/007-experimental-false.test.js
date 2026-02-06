9fbb57641773cfe512ba246eb149b824e5584c1d
Claas Augner <caugner@mozilla.com>
2026-02-06 17:33:01 +0100

    refactor(test): migrate from mocha to node:test/assert

--- scripts/migrations/007-experimental-false.test.js
+++ scripts/migrations/007-experimental-false.test.js
@@ -3,6 +3,7 @@
 
 /** @import {CompatStatement} from '../../types/types.js' */
 
+import { describe, it } from 'node:test';
 import assert from 'node:assert/strict';
 
 import { fixExperimental } from './007-experimental-false.js';
