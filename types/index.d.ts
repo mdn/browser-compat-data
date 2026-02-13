b7377874dd87e776541fc10c176c92190b288806
Claas Augner <caugner@mozilla.com>
2026-02-14 00:08:16 +0100

    chore(types): remove manual types

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,57 +1,7 @@
 /* This file is a part of @mdn/browser-compat-data
  * See LICENSE file for more information. */
 
-import type {
-  BrowserName,
-  BrowserStatement,
-  CompatData,
-  CompatStatement,
-  Identifier,
-  SimpleSupportStatement,
-  SupportStatement,
-} from './types.js';
-
-export type {
-  Browsers,
-  BrowserName,
-  BrowserStatement,
-  BrowserStatus,
-  FlagStatement,
-  MetaBlock,
-  ReleaseStatement,
-  SimpleSupportStatement,
-  StatusBlock,
-  VersionValue,
-} from './types.js';
-
-export type InternalCompatData = Omit<CompatData, '__meta'>;
-
-export type InternalSupportStatement = SupportStatement | 'mirror';
-export type InternalSimpleSupportStatement = SimpleSupportStatement | 'mirror';
-
-export type InternalSupportBlock = Partial<
-  Record<BrowserName, InternalSupportStatement>
->;
-
-export interface InternalCompatStatement extends Omit<
-  CompatStatement,
-  'source_file' | 'support'
-> {
-  support: InternalSupportBlock;
-}
-
-export type InternalIdentifier =
-  | {
-      __compat: InternalCompatStatement;
-      [k: string]: InternalIdentifier;
-    }
-  | {
-      __compat?: InternalCompatStatement;
-      [k: string]: InternalIdentifier;
-    }
-  | {
-      [k: string]: InternalIdentifier;
-    };
+export type * from './types.js';
 
 export type DataType =
   | CompatData
