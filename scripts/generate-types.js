c43fb4120338de86af06e7281385de106d362fd8
Claas Augner <caugner@mozilla.com>
2026-02-17 20:49:27 +0100

    chore(scripts/generate-types): clean up old types

--- scripts/generate-types.js
+++ scripts/generate-types.js
@@ -1,6 +1,8 @@
 /* This file is a part of @mdn/browser-compat-data
  * See LICENSE file for more information. */
 
+import { rm } from 'node:fs/promises';
+
 import esMain from 'es-main';
 
 import compileInternal from './generate-internal-types.js';
@@ -8,8 +10,20 @@ import compilePublic from './generate-public-types.js';
 
 /* c8 ignore start */
 
+/**
+ * Cleans up old types.
+ */
+const cleanupObsolete = async () => {
+  const path = new URL('../types/types.d.ts', import.meta.url);
+  try {
+    await rm(path);
+  } catch {
+    // Ignore.
+  }
+};
+
 if (esMain(import.meta)) {
-  await Promise.all([compileInternal(), compilePublic()]);
+  await Promise.all([compileInternal(), compilePublic(), cleanupObsolete()]);
 }
 
 /* c8 ignore stop */
