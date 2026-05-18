5f1e1a3da080b01f42c400e631d57e6077025b4b
Danyal Ahmed <58849388+danyalahmed1995@users.noreply.github.com>
2026-05-18 07:59:04 +0500

    fix(lint): fail lint:fix when unfixable errors remain

--- lint/fix.js
+++ lint/fix.js
@@ -25,6 +25,7 @@ import fixStatus from './fixer/status.js';
 import fixMirror from './fixer/mirror.js';
 import fixOverlap from './fixer/overlap.js';
 import fixStandardTrackExceptions from './fixer/standard-track-exceptions.js';
+import lint from './lint.js';
 import { IS_WINDOWS } from './utils.js';
 
 /** @import {Stats} from 'node:fs' */
@@ -145,6 +146,7 @@ if (esMain(import.meta)) {
   const { files = dataFolders, only } = argv;
 
   await main(files, { only });
+  process.exit((await lint(files)) ? 1 : 0);
 }
 
 export default load;
